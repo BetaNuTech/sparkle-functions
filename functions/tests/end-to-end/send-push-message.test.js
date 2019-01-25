925d10e4ceabda34ee8bb8f70190ae57b0ea61c7
Matthew Jensen <matt@jwc.works>
2019-01-24 19:15:49 -0600

    #11 add e2e test for sendPushMessage function to test message removal

--- functions/tests/end-to-end/send-push-message.test.js
+++ functions/tests/end-to-end/send-push-message.test.js
@@ -1,20 +1,41 @@
+const co = require('co');
 const test = require('firebase-functions-test')({
   databaseURL: 'https://test-sapphire-inspections-8a9e3.firebaseio.com',
   storageBucket: 'test-sapphire-inspections-8a9e3.appspot.com',
   projectId: 'test-sapphire-inspections-8a9e3',
-}, '../../../auth.json');
-const cloudFunctions = require('../../index');
+}, '../auth.json');
+const sinon = require('sinon');
+const admin = require('firebase-admin');
+const { expect } = require('chai');
+const uuid = require('../../test-helpers/uuid');
+admin.initializeApp();
+const db = admin.database();
 
 describe('Send Push Message', () => {
-  it('should remove a message after sending to its\' recipients', (done) => {
-    // TODO create mock /pushMessages w/ recipientId:
-    // const messageSnap = test.firestore.makeDocumentSnapshot({ recipientId: '123' ... }, '/pushMessages/<id>');
-    // TODO create mock /registrationToken for recipientId
-    // const tokenSnap = test.firestore.makeDocumentSnapshot({ '123': { <id>: true } }, '/registrationToken/123');
-    // const wrapped = test.wrap(cloudFunctions.sendPushMessage);
-    // wrapped(messageSnap).then(() => {
-      // TODO test reg token removed
-    // });
-    done();
+  var cloudFunctions, oldDatabase;
+
+  before(() => {
+    // Stub admin.initializeApp to avoid live database access
+    if (!admin.initializeApp.isSinonProxy) {
+      adminInitStub = sinon.stub(admin, 'initializeApp').returns({ database: () => db });
+      oldDatabase = admin.database;
+      Object.defineProperty(admin, 'database', { writable: true, value: () => db });
+    }
+    cloudFunctions = require('../../index');
   });
+  after(() => admin.database = oldDatabase);
+
+  it('should remove a message after sending to its\' recipients', () => co(function *() {
+    const msgId = uuid();
+    const messagePath = `/sendMessages/${msgId}`;
+    const messageData = { createdAt: Date.now() / 1000, message: 'message', title: 'title', recipientId: uuid() };
+    yield db.ref(messagePath).set(messageData);
+    const message = yield db.ref(messagePath).once('value');
+    const messageSnap = test.makeChange(null, message);
+    const wrapped = test.wrap(cloudFunctions.sendPushMessage);
+
+    yield wrapped(messageSnap, { params: { objectId: msgId } });
+    const actual = yield db.ref(messagePath).once('value');
+    expect(actual.exists()).to.equal(false);
+  }));
 });
