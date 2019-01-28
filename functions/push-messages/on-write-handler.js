365db514803b15c84d1ddd2bb5897dbaea4d9f75
Matthew Jensen <matt@jwc.works>
2019-01-28 12:37:11 -0600

    #16 consolodate push-message functions and abstract push-message modules

---
+++ functions/push-messages/on-write-handler.js
@@ -0,0 +1,31 @@
+const co = require('co');
+const sendToRecipient = require('./send-to-recipient');
+
+/**
+ * Factory for push message on write handler
+ * @param  {firebaseAdmin.database} - Firebase Admin DB instance
+ * @param  {firebaseAdmin.messaging} - Firebase Admin messaging service instance
+ * @return {Function} - push messages onWrite handler
+ */
+module.exports = function createPushMessagesOnWriteHandler(db, messaging) {
+  return (change, event) => co(function *() {
+    // Exit when the data is deleted.
+    if (!change.after.exists()) {
+        return false;
+    }
+
+    const pushMessage = change.after.val();
+    const recipientId = pushMessage.recipientId;
+    const objectId = event.params.objectId;
+
+    yield sendToRecipient(
+      db,
+      messaging,
+      recipientId,
+      objectId,
+      pushMessage
+    );
+
+    return db.ref(`/sendMessages/${objectId}`).remove();
+  });
+}
---
+++ functions/push-messages/send-to-recipient.js
@@ -0,0 +1,61 @@
+const co = require('co');
+const log = require('../utils/logger');
+
+/**
+ * Send a given push notifcation message to a recipient ID
+ * @param {firebaseAdmin.database} db
+ * @param {firebaseAdmin.messaging} messaging
+ * @param {String} recipientId
+ * @param {String} messageId
+ * @param {Object} pushMessage
+ * @return {Promise} - resolves {String[]} recipient's registration tokens
+ */
+module.exports = function sendToRecipient(db, messaging, recipientId, messageId, pushMessage = {}) {
+  return co(function *() {
+    var registrationTokens = [];
+    const dataSnapshot = yield db.ref('/registrationTokens').child(recipientId).once('value');
+
+    if (!dataSnapshot.exists()) {
+      // Recipient has no tokens
+      return registrationTokens;
+    }
+
+    // These registration tokens come from the client FCM SDKs.
+    if (dataSnapshot.hasChildren()) {
+      dataSnapshot.forEach((childSnapshot) => {
+        // key will be "ada" the first time and "alan" the second time
+        // childData will be the actual contents of the child
+        registrationTokens.push(childSnapshot.key);
+      });
+    } else {
+      registrationTokens.push(dataSnapshot.key);
+    }
+
+    // See the "Defining the message payload" section above for details
+    // on how to define a message payload.
+    const payload = {
+      notification: {
+        title: pushMessage.title,
+        body: pushMessage.message,
+        icon: 'https://s3.us-east-2.amazonaws.com/sapphireinspections/assets/app_icon_192.png'
+      }
+    };
+
+    // Set the message as high priority and have it expire after 24 hours.
+    // var options = {
+    //   priority: "high",
+    //   timeToLive: 60 * 60 * 24
+    // };
+
+    // Send a message to the device corresponding to the provided
+    // registration token with the provided options.
+    try {
+      const response = yield messaging.sendToDevice(registrationTokens, payload, {});
+      log.info(`Successfully sent message: ${response}`);
+    } catch (e) {
+      log.error(`Error sending message: ${e}`);
+    }
+
+    return registrationTokens;
+  });
+}
