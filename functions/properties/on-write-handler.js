d166da43352e406c30bfe9a960aadce805802695
Matthew Jensen <matt@jwc.works>
2019-01-28 14:04:07 -0600

    #16 remove redundant code in propertyWrite

--- functions/properties/on-write-handler.js
+++ functions/properties/on-write-handler.js
@@ -30,24 +30,6 @@ module.exports = function createOnWriteHandler(db) {
       return Promise.resolve({});
     }
 
-    var templatesPrevious = null;
-    if (change.before.exists()) {
-      templatesPrevious = change.before.val().templates;
-    }
-    var templatesCurrent = change.after.val().templates;
-
-    // Find removed templates and remove them
-    // TODO: is this necessary???
-    if (templatesPrevious != null) {
-      var templatesRemoved = findRemovedKeys(templatesPrevious, templatesCurrent); // Array of template keys
-      if (templatesRemoved.length > 0) {
-        log.info('templates removed count: ', templatesRemoved.length);
-        templatesRemoved.forEach(function(templateKey) {
-          db.ref('/propertyTemplates').child(propertyId).child(templateKey).remove();
-        });
-      }
-    }
-
-    return propertyTemplates.processWrite(db, propertyId, templatesCurrent);
+    return propertyTemplates.processWrite(db, propertyId, change.after.val().templates);
   };
 }
