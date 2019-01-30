9dfec76a47345564f7a21661497926e1a56d6667
Matthew Jensen <matt@jwc.works>
2019-01-30 17:39:38 -0600

    #18 add failing test for template category deletion of template list categories

--- functions/tests/end-to-end/template-category-delete.test.js
+++ functions/tests/end-to-end/template-category-delete.test.js
@@ -57,4 +57,38 @@ describe('Template Category Delete', () => {
     expect(actualTmpl2Cat.val()).to.equal(null, 'removed template 2 relationship');
     expect(actualTmpl3Cat.val()).to.equal(tmpl3Data.category, 'template 3 relationship unchanged');
   }));
+
+  it('should disassociate all /templatesList belonging to the template category', () => co(function *() {
+    const tmpl1Id = uuid();
+    const tmpl2Id = uuid();
+    const tmpl3Id = uuid();
+    const categoryId = uuid();
+    const tmpl1Data = { name: `test${tmpl1Id}`, description: `desc${tmpl1Id}`, category: categoryId }; // related
+    const tmpl2Data = { name: `test${tmpl2Id}`, description: `desc${tmpl2Id}`, category: categoryId }; // related
+    const tmpl3Data = { name: `test${tmpl3Id}`, description: `desc${tmpl3Id}`, category: uuid() }; // unrelated
+
+    // Setup database
+    yield db.ref(`/templates/${tmpl1Id}`).set(tmpl1Data); // add template #1
+    yield db.ref(`/templatesList/${tmpl1Id}`).set(tmpl1Data); // add template #1 proxy
+    yield db.ref(`/templates/${tmpl2Id}`).set(tmpl2Data); // add template #2
+    yield db.ref(`/templatesList/${tmpl2Id}`).set(tmpl2Data); // add template #2 proxy
+    yield db.ref(`/templates/${tmpl3Id}`).set(tmpl3Data); // add template #3
+    yield db.ref(`/templatesList/${tmpl3Id}`).set(tmpl3Data); // add template #2 proxy
+    yield db.ref(`/templateCategories/${categoryId}`).set({ name: `category${categoryId}` }); // add category
+    const deleteSnap = yield db.ref(`/templateCategories/${categoryId}`).once('value');
+
+    // Execute
+    const wrapped = test.wrap(cloudFunctions.templateCategoryDelete);
+    yield wrapped(deleteSnap, { params: { objectId: categoryId } });
+
+    // Test result
+    const actualTmpl1Cat = yield db.ref(`/templatesList/${tmpl1Id}/category`).once('value');
+    const actualTmpl2Cat = yield db.ref(`/templatesList/${tmpl2Id}/category`).once('value');
+    const actualTmpl3Cat = yield db.ref(`/templatesList/${tmpl3Id}/category`).once('value');
+
+    // Assertions
+    expect(actualTmpl1Cat.val()).to.equal(null, 'removed template 1 proxy relationship');
+    expect(actualTmpl2Cat.val()).to.equal(null, 'removed template 2 proxy relationship');
+    expect(actualTmpl3Cat.val()).to.equal(tmpl3Data.category, 'template 3 proxy relationship unchanged');
+  }));
 });
