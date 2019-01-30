320a34714fbb26709c47706ebb0cd4d0851ae95d
Matthew Jensen <matt@jwc.works>
2019-01-30 16:56:53 -0600

    #18 add templates list write unit tests

--- functions/templates/list.spec.js
+++ functions/templates/list.spec.js
@@ -8,5 +8,29 @@ describe('Templates List', () => {
       const actual = write(createDatabaseStub().value(), 'test', {}, null);
       expect(actual).to.be.an.instanceof(Promise);
     });
+
+    it('should resolve `null` on template deletion', () => {
+      return write(
+        createDatabaseStub().value(),
+        'test',
+        { name: 'test' },
+        null // removed
+      ).then((actual) =>
+        expect(actual).to.equal(null)
+      )
+    });
+
+    it('should resolve upserted data on template upsert', () => {
+      const expected = { name: 'test-2', description: 'desc-2' };
+
+      return write(
+        createDatabaseStub().value(),
+        'test',
+        { name: 'test', description: 'desc' },
+        expected // update
+      ).then((actual) =>
+        expect(actual).to.deep.equal(expected)
+      )
+    });
   });
 });
