31b532275fa0a3cc876754b9e3f09c46af89ff57
Matthew Jensen <matt@jwc.works>
2019-02-01 14:54:43 -0600

    #12 support writing auth credentials from environment variable when non-existent

--- functions/test/end-to-end/setup.js
+++ functions/test/end-to-end/setup.js
@@ -1,9 +1,16 @@
-console.log();
+const fs = require('fs');
+const AUTH_FILE_PATH = '../auth.json';
+
+// Write firbase auth file from environment if non-existent
+if (process.env.FIREBASE_FUNCTIONS_AUTH && !fs.existsSync(AUTH_FILE_PATH)) {
+  fs.writeFileSync(AUTH_FILE_PATH, Buffer.from(process.env.FIREBASE_FUNCTIONS_AUTH, 'hex'));
+}
+
 const test = require('firebase-functions-test')({
   databaseURL: 'https://test-sapphire-inspections-8a9e3.firebaseio.com',
   storageBucket: 'test-sapphire-inspections-8a9e3.appspot.com',
   projectId: 'test-sapphire-inspections-8a9e3',
-}, '../auth.json');
+}, AUTH_FILE_PATH);
 const sinon = require('sinon');
 const admin = require('firebase-admin');
 
