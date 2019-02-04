7c8725c444c2a6e5ca803a2c539e0daf8fe01731
Matthew Jensen <matt@jwc.works>
2019-02-04 14:26:43 -0600

    #17 add test assertions for /propertyTemplatesList

--- functions/test/end-to-end/property-write.test.js
+++ functions/test/end-to-end/property-write.test.js
@@ -4,10 +4,10 @@ const uuid = require('../../test-helpers/uuid');
 const { cleanDb } = require('../../test-helpers/firebase');
 const { db, test, cloudFunctions } = require('./setup');
 
-describe('Property Templates Write', () => {
+describe('Property Write', () => {
   afterEach(() => cleanDb(db));
 
-  it('should remove all a property\'s `/propertyTemplates` when it gets deleted', () => co(function *() {
+  it('should remove all a deleted property\'s template proxies', () => co(function *() {
     const tmplId = uuid();
     const propertyId = uuid();
     const templateData = { name: `test${tmplId}`};
@@ -15,7 +15,8 @@ describe('Property Templates Write', () => {
     // Setup database
     yield db.ref(`/properties/${propertyId}`).set({ name: 'test', templates: { [tmplId]: true } }); // Add property with a template    yield db.ref('/templates').set(expected);
     yield db.ref(`/templates/${tmplId}`).set(templateData); // Add template
-    yield db.ref(`/propertyTemplates/${propertyId}/${tmplId}`).set(templateData); // Add propertyTemplate proxy record
+    yield db.ref(`/propertyTemplates/${propertyId}/${tmplId}`).set(templateData); // Add propertyTemplates
+    yield db.ref(`/propertyTemplatesList/${propertyId}/${tmplId}`).set(templateData); // Add propertyTemplatesList
 
     const propertyBeforeSnap = yield db.ref(`/properties/${propertyId}`).once('value'); // Get before property
     yield db.ref(`/properties/${propertyId}`).remove(); // Remove property
@@ -28,12 +29,14 @@ describe('Property Templates Write', () => {
 
     // Test result
     const actual = yield db.ref(`/propertyTemplates/${propertyId}`).once('value');
+    const actualList = yield db.ref(`/propertyTemplatesList/${propertyId}`).once('value');
 
     // Assertions
-    expect(actual.exists()).to.equal(false);
+    expect(actual.exists()).to.equal(false, 'removed /propertyTemplates proxy');
+    expect(actualList.exists()).to.equal(false, 'removed /propertyTemplatesList proxy');
   }));
 
-  it('should remove from /propertyTemplates when a template is removed from a property', () => co(function *() {
+  it('should remove a template\'s property proxies when it is disassociated from that property', () => co(function *() {
     const tmplId1 = uuid();
     const tmplId2 = uuid();
     const propertyId = uuid();
@@ -46,7 +49,9 @@ describe('Property Templates Write', () => {
     yield db.ref(`/properties/${propertyId}/templates/${tmplId2}`).remove(); // Remove 2nd template
     const propertyAfterSnap = yield db.ref(`/properties/${propertyId}`).once('value'); // Get after templates
     yield db.ref(`/propertyTemplates/${propertyId}/${tmplId1}`).set(expected[tmplId1]);
+    yield db.ref(`/propertyTemplatesList/${propertyId}/${tmplId1}`).set(expected[tmplId1]);
     yield db.ref(`/propertyTemplates/${propertyId}/${tmplId2}`).set({ name: `test${tmplId2}`});
+    yield db.ref(`/propertyTemplatesList/${propertyId}/${tmplId2}`).set({ name: `test${tmplId2}`});
 
     // Execute
     const changeSnap = test.makeChange(propertyBeforeSnap, propertyAfterSnap);
@@ -55,12 +60,14 @@ describe('Property Templates Write', () => {
 
     // Test result
     const actual = yield db.ref(`/propertyTemplates/${propertyId}`).once('value');
+    const actualList = yield db.ref(`/propertyTemplatesList/${propertyId}`).once('value');
 
     // Assertions
-    expect(actual.val()).to.deep.equal(expected);
+    expect(actual.val()).to.deep.equal(expected, 'has /propertyTemplates proxy');
+    expect(actualList.val()).to.deep.equal(expected, 'has /propertyTemplatesList proxy');
   }));
 
-  it('should always upsert `propertyTemplates` when a property has any template relationships', () => co(function *() {
+  it('should upsert template property proxies when a property has template relationships', () => co(function *() {
     const tmplId1 = uuid();
     const tmplId2 = uuid();
     const propertyId = uuid();
@@ -70,10 +77,10 @@ describe('Property Templates Write', () => {
     };
 
     // Setup database
-    // Add property with templates
-    yield db.ref('/templates').set(expected);
+    yield db.ref('/templates').set(expected); // Add property's templates
     yield db.ref(`/properties/${propertyId}`).set({ name: 'test', templates: { [tmplId1]: true } }); // Only has 1st template
     yield db.ref(`/propertyTemplates/${propertyId}/${tmplId1}`).set(expected[tmplId1]); // Add 1st template proxy record
+    yield db.ref(`/propertyTemplatesList/${propertyId}/${tmplId1}`).set(expected[tmplId1]); // Add 1st template list proxy record
     const propertyBeforeSnap = yield db.ref(`/properties/${propertyId}`).once('value'); // Get before templates
     yield db.ref(`/properties/${propertyId}/templates/${tmplId2}`).set(true); // Associate 2nd template w/ property
     const propertyAfterSnap = yield db.ref(`/properties/${propertyId}`).once('value'); // Get after templates
@@ -85,8 +92,10 @@ describe('Property Templates Write', () => {
 
     // Test result
     const actual = yield db.ref(`/propertyTemplates/${propertyId}`).once('value');
+    const actualList = yield db.ref(`/propertyTemplatesList/${propertyId}`).once('value');
 
     // Assertions
-    expect(actual.val()).to.deep.equal(expected);
+    expect(actual.val()).to.deep.equal(expected, 'has /propertyTemplates proxy');
+    expect(actualList.val()).to.deep.equal(expected, 'has /propertyTemplatesList proxy');
   }));
 });
--- functions/test/end-to-end/templates-sync.test.js
+++ functions/test/end-to-end/templates-sync.test.js
@@ -7,7 +7,7 @@ const { db, test, cloudFunctions } = require('./setup');
 describe('Templates Sync', () => {
   afterEach(() => cleanDb(db));
 
-  it('should update an existing template\'s data in /propertyTemplates', () => co(function *() {
+  it('should update all existing proxy template\'s data', () => co(function *() {
     const tmplId = uuid();
     const propertyId = uuid();
     const expected = { description: `desc${tmplId}`, name: `new${tmplId}` };
@@ -17,44 +17,53 @@ describe('Templates Sync', () => {
     yield db.ref(`/templates/${tmplId}`).set(expected);
     yield db.ref(`/properties/${propertyId}`).set(propertyData);
     yield db.ref('/propertyTemplates').set({ [propertyId]: { [tmplId]: { name: 'old' } } }); // must exist
+    yield db.ref('/propertyTemplatesList').set({ [propertyId]: { [tmplId]: { name: 'old' } } }); // must exist
 
     // Execute
     yield test.wrap(cloudFunctions.templatesSync)();
 
     // Test results
     const actual = yield db.ref(`/propertyTemplates/${propertyId}/${tmplId}`).once('value');
+    const actualList = yield db.ref(`/propertyTemplatesList/${propertyId}/${tmplId}`).once('value');
 
     // Assertions
-    expect(actual.val()).to.deep.equal(expected);
+    expect(actual.val()).to.deep.equal(expected, 'updated /propertyTemplates proxy');
+    expect(actualList.val()).to.deep.equal(expected, 'updated /propertyTemplatesList proxy');
   }));
 
-  it('should remove /propertyTemplates no longer associated with property', () => co(function *() {
+  it('should remove a property\'s template proxies no longer associated with property', () => co(function *() {
     const oldTmplId = uuid();
     const currTmplId = uuid();
     const propertyId = uuid();
     const templateData = { name: `remove${oldTmplId}`, description: `remove${oldTmplId}` };
     const propertyData = { templates: { [currTmplId]: true } }; // not associated w/ oldTmplId
-
-    // Setup database
-    yield db.ref(`/templates/${oldTmplId}`).set(templateData);
-    yield db.ref(`/properties/${propertyId}`).set(propertyData);
-    yield db.ref('/propertyTemplates').set({
+    const proxyTmplData = {
       [propertyId]: {
         [oldTmplId]: templateData, // must exist
         [currTmplId]: { name: 'current' }
       }
-    });
+    };
+
+    // Setup database
+    yield db.ref(`/templates/${oldTmplId}`).set(templateData);
+    yield db.ref(`/properties/${propertyId}`).set(propertyData);
+    yield db.ref('/propertyTemplates').set(proxyTmplData);
+    yield db.ref('/propertyTemplatesList').set(proxyTmplData);
 
     // Execute
     yield test.wrap(cloudFunctions.templatesSync)();
 
     // Test results
     const removedtmpl = yield db.ref(`/propertyTemplates/${propertyId}/${oldTmplId}`).once('value');
+    const removedtmplList = yield db.ref(`/propertyTemplatesList/${propertyId}/${oldTmplId}`).once('value');
     const currentTmpl = yield db.ref(`/propertyTemplates/${propertyId}/${currTmplId}`).once('value');
+    const currentTmplList = yield db.ref(`/propertyTemplatesList/${propertyId}/${currTmplId}`).once('value');
 
     // Assertions
-    expect(removedtmpl.exists()).to.equal(false, 'removed disassociated propertyTemplate');
-    expect(currentTmpl.exists()).to.equal(true, 'kept associated propertyTemplate');
+    expect(removedtmpl.exists()).to.equal(false, 'removed disassociated /propertyTemplates');
+    expect(removedtmplList.exists()).to.equal(false, 'removed disassociated /propertyTemplatesList');
+    expect(currentTmpl.exists()).to.equal(true, 'kept associated /propertyTemplates');
+    expect(currentTmplList.exists()).to.equal(true, 'kept associated /propertyTemplatesList');
   }));
 
   it('should add missing records in /templatesList', () => co(function *() {
