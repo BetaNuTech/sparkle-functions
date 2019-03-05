4d81fc338248ab89f9906f498faafdf0994de48c
Matthew Jensen <matt@jwc.works>
2019-03-05 14:15:42 -0600

    #55 migrate inspection create and upload to the inspections/pdf-report module

---
+++ functions/inspections/pdf-reports/create-and-upload-inspection-pdf/index.js
@@ -0,0 +1,61 @@
+const co = require('co');
+const fs = require('fs');
+const assert = require('assert');
+const {promisify} = require('util');
+const JsPDF = require('./js-pdf');
+const inspectionPdf = require('./inspection-pdf');
+const inspectionUpload = require('./inspection-upload');
+const readFile = promisify(fs.readFile);
+
+/**
+ * Generate a PDF from a property and inspection record
+ * @type {Coroutine}
+ * @param {Object} property
+ * @param {Object} inspection
+ * @return {Promise} - resolve {String} report download url
+ */
+module.exports = co(function *(property, inspection) {
+  assert('has property', Boolean(property));
+  assert('has inspection with id', Boolean(inspection) && inspection.id);
+
+  const src = inspectionPdf(inspection, property);
+  const steps = src.toSteps();
+  const pdf = new JsPDF({format: 'letter'});
+  // let pdfFile;
+
+  // Add steps to PDF
+  for (let i = 0; i < steps.length; i++) {
+    Object.keys(steps[i]).forEach(command => {
+      let args = steps[i][command];
+      if (!Array.isArray(args)) args = [args];
+      pdf[command](...args);
+    });
+  }
+
+  // Convert PDF source to tmp file
+  const output = toBuffer(pdf.output('arraybuffer'));
+  // pdfFile = tmpFile(output, src.filename);
+  // const tmpFilePath = await pdfFile.create();
+
+  // Upload tmp PDF file to S3
+  return inspectionUpload(
+    output,
+    `reports/${inspection.id}/${src.filename}`
+  );
+})();
+
+/**
+ * Convert Array Buffer to Buffer
+ * @param  {ArrayBuffer} ab
+ * @return {Buffer}
+ */
+function toBuffer(ab) {
+  const buf = new Buffer(ab.byteLength);
+  const view = new Uint8Array(ab);
+
+  for (let i = 0; i < buf.length; ++i) {
+    buf[i] = view[i];
+  }
+
+  return buf;
+}
---
+++ functions/inspections/pdf-reports/create-and-upload-inspection-pdf/inspection-upload.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const assert = require('assert');
+const s3Client = require('../../../utils/s3-client');
+const CONFIG = require('../../../config');
+
+/**
+ * Upload a local file to the inspection report S3 bucket
+ * @-param  {String} filePath local temporary file path
+ * @param  {Buffer} file
+ * @param  {String} destPath
+ * @return {Promise} - resolves {String} Url to inspection PDF
+ */
+module.exports = function uploadInspectionPromise(file, destPath) {
+  assert('has file buffer', file instanceof Buffer);
+  assert('has upload string', destPath && typeof destPath === 'string');
+
+  return new Promise((resolve, reject) => {
+    s3Client.putObject(
+      {
+        Body: file,
+        Bucket: CONFIG.s3.inspectionReportBucket,
+        Key: destPath,
+        ContentType: 'application/pdf'
+      },
+      (putErr, result) => {
+        if (putErr) return reject(putErr);
+        resolve(
+          `https://${CONFIG.s3.inspectionReportBucket}.${
+            CONFIG.s3.endpoint
+          }/${uploadPath}`
+        );
+      }
+    );
+
+    // fs.readFile(filePath, (err, file) => {
+      // if (err) return reject(err);
+      // Removed
+    // });
+  });
+}
