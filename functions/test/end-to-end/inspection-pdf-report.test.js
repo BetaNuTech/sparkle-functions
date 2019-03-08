8d9536d49ab393e411e65e36e6471dc9177acf6e
Matthew Jensen <matt@jwc.works>
2019-03-08 14:47:27 -0600

    #58 add tests for inspectionReportURL

--- functions/test/end-to-end/inspection-pdf-report.test.js
+++ functions/test/end-to-end/inspection-pdf-report.test.js
@@ -80,6 +80,29 @@ describe('Inspection PDF Report', () => {
     expect(actual.val()).to.be.a('number');
   });
 
+  it('should add an `inspectionReportURL` to inspection', async function() {
+    // Setup database
+    const inspectionData = Object.assign({}, INSPECTION_DATA);
+    delete inspectionData.inspectionReportURL;
+    await db.ref(`/inspections/${INSP_ID}`).set(inspectionData); // Add inspection
+    await db.ref(`/properties/${PROPERTY_ID}`).set(PROPERTY_DATA); // Add property
+
+    // Execute
+    const app = createApp(db, { sendToDevice: () => Promise.resolve() });
+    const response = await request(app)
+      .get(`/${PROPERTY_ID}/${INSP_ID}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    // Get Result
+    const actual = await db.ref(`/inspections/${INSP_ID}/inspectionReportURL`).once('value');
+    const expected = response.body.inspectionReportURL;
+
+    // Assertions
+    expect(actual.val()).to.equal(expected);
+  });
+
   it('should update `inspectionReportStatus` on inspection', async function() {
     // Setup database
     await db.ref(`/inspections/${INSP_ID}`).set(INSPECTION_DATA); // Add inspection
