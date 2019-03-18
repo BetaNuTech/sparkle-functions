134e8051b1b866580062297cc9c33cd3273534a6
Matthew Jensen <matt@jwc.works>
2019-03-18 17:50:12 -0500

    #67 add function to handle inspection proxy cleanup

---
+++ functions/inspections/cron/cleanup-proxy-orphans.js
@@ -0,0 +1,24 @@
+const log = require('../../utils/logger');
+const adminUtils = require('../../utils/firebase-admin');
+const { isInspectionWritable } = require('../process-write/utils');
+const completedInspectionsList = require('../process-write/completed-inspections-list');
+const { isInspectionOutdated } = require('./utils');
+
+const LOG_PREFIX = 'inspections: cron: cleanup-proxy-orphans:';
+
+/**
+ * Cleanup all outdated inspecton proxies
+ * @param  {string} topic
+ * @param  {functions.pubsub} pubsub
+ * @param  {firebaseadmin.database} db
+ * @return {functions.cloudfunction}
+ */
+module.exports = function createCleanupProxyOrphansHandler(topic = '', pubsub, db) {
+  return pubsub
+  .topic(topic)
+  .onPublish(async function syncCleanupProxyOrphansHandler() {
+    const updates = {};
+    log.info(`${LOG_PREFIX} received ${Date.now()}`);
+    return updates;
+  });
+}
