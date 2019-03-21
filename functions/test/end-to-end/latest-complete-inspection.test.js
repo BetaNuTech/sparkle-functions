dae060007b22e9615d571a3f6a6581004fb7bcfb
Matthew Jensen <matt@jwc.works>
2019-03-21 18:23:04 -0500

    #78 rejects requests for properties without completed inspections

--- functions/test/end-to-end/latest-complete-inspection.test.js
+++ functions/test/end-to-end/latest-complete-inspection.test.js
@@ -8,28 +8,39 @@ const { db } = require('./setup');
 describe('Latest Complete Inspection', () => {
   afterEach(() => cleanDb(db));
 
-  it('should reject request without cobalt code', function(done) {
+  it('should reject request without cobalt code', async function() {
     // Execute & Get Result
     const app = createApp(db);
-
-    request(app)
+    const response = await request(app)
       .get('/')
-      .expect(400)
-      .end((err, res) => {
-        expect(res.text.toLowerCase()).to.have.string('missing parameters');
-        done();
-      });
+      .expect(400);
+
+    expect(response.text.toLowerCase()).to.have.string('missing parameters');
   });
 
-  it('should reject request without property matching cobalt code', function(done) {
+  it('should reject request without property matching cobalt code', async function() {
     // Execute & Get Result
     const app = createApp(db);
-    request(app)
+    const response = await request(app)
       .get('/?cobalt_code=1')
       .expect(404)
-      .end((err, res) => {
-        expect(res.text.toLowerCase()).to.have.string('not found');
-        done();
-      });
+
+    expect(response.text.toLowerCase()).to.have.string('not found');
+  });
+
+  it('should reject request when property has no completed inspections', async function() {
+    const code = uuid();
+    const propertyId = uuid();
+
+    // Setup database
+    await db.ref(`/properties/${propertyId}`).set({name: `test${propertyId}`, code});
+
+    // Execute & Get Result
+    const app = createApp(db);
+    const response = await request(app)
+      .get(`/?cobalt_code=${code}`)
+      .expect(404)
+
+    expect(response.text.toLowerCase()).to.have.string('no inspections exist');
   });
 });
