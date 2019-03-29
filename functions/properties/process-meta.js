80a09045397cf6b86b5eb7f8f808cb575e2e0d4c
Matthew Jensen <matt@jwc.works>
2019-03-29 12:52:52 -0500

    #80 refactor property meta processing to composition of update functions

--- functions/properties/process-meta.js
+++ functions/properties/process-meta.js
@@ -1,6 +1,10 @@
+const pipe = require('lodash/fp/flow');
 const log = require('../utils/logger');
 
-const LOG_PREFIX = 'inspections: process-property-meta:';
+const LOG_PREFIX = 'properties: process-meta:';
+
+// Pipeline of steps to update metadata
+const propertyMetaUpdates = pipe([updateNumOfInspections, updateLastInspectionAttrs]);
 
 /**
  * Process changes to a property's metadata when it's
@@ -12,62 +16,75 @@ const LOG_PREFIX = 'inspections: process-property-meta:';
 module.exports = async function processMeta(db, propertyId) {
   const updates = Object.create(null);
 
-  // Pull all inspections for the same property
-  const inspectionsSnapshot = await db.ref('/inspections').orderByChild('property').equalTo(propertyId).once('value');
-
-  if (!inspectionsSnapshot.exists()) {
-    return updates;
-  }
-
   try {
-    var latestInspection = null;
-    var latestInspectionTmp = null;
-    var numOfInspectionsCompleted = 0;
-    const inspections = [];
+    // Pull all property's inspections
+    const inspectionsSnapshot = await db.ref('/inspections').orderByChild('property').equalTo(propertyId).once('value');
+    const inspectionsData = inspectionsSnapshot.exists() ? inspectionsSnapshot.val() : {};
+    const inspections = Object.keys(inspectionsData).map(inspId => Object.assign({id: inspId}, inspectionsData[inspId]));
 
-    if (inspectionsSnapshot.hasChildren()) {
-      inspectionsSnapshot.forEach((childSnapshot) => {
-        // key will be 'ada' the first time and 'alan' the second time
-        latestInspectionTmp = childSnapshot.val();
-
-        // childData will be the actual contents of the child
-        if (latestInspectionTmp.inspectionCompleted) {
-          inspections.push(latestInspectionTmp);
-          numOfInspectionsCompleted++;
-        }
-      });
-
-      if (inspections.length) {
-        latestInspection = inspections.sort((a, b) => b.creationDate - a.creationDate)[0]; // DESC
-      }
-    } else {
-      latestInspectionTmp = inspectionsSnapshot.val();
-
-      if (latestInspectionTmp.inspectionCompleted) {
-        latestInspection = latestInspectionTmp;
-        numOfInspectionsCompleted++;
-      }
+    if (!inspections.length) {
+      return updates;
     }
 
-    // Update `numOfInspections` for the property
-    await db.ref(`/properties/${propertyId}/numOfInspections`).set(numOfInspectionsCompleted);
-    log.info(`${LOG_PREFIX} property ${propertyId}: numOfInspections updated`);
-    updates[`/properties/${propertyId}/numOfInspections`] = 'updated';
+    // Collect updates to write to propety's metadata attrs
+    propertyMetaUpdates({
+      propertyId,
+      inspections,
+      updates
+    });
 
-    if (latestInspection) {
-      await db.ref(`/properties/${propertyId}`).update({
-        lastInspectionScore: latestInspection.score,
-        lastInspectionDate: latestInspection.creationDate
-      });
-
-      log.info(`${LOG_PREFIX} property ${propertyId}: lastInspectionScore & lastInspectionDate updated`);
-      updates[`/properties/${propertyId}/lastInspectionScore`] = 'updated';
-      updates[`/properties/${propertyId}/lastInspectionDate`] = 'updated';
+    // Atomically write each metadata update
+    const updatePaths = Object.keys(updates);
+    for (let i = 0; i < updatePaths.length; i++) {
+      const path = updatePaths[i];
+      await db.ref(path).set(updates[path]);
     }
-  } catch(e) {
-    log.error(`${LOG_PREFIX} property ${propertyId}: Unable to access inspections ${e}`);
+
+    log.info(`${LOG_PREFIX} successfully updated property ${propertyId} metadata`);
+  } catch (e) {
+    log.error(`${LOG_PREFIX} failed updating property ${propertyId} metadata ${e}`);
     return null;
   }
 
   return updates;
+}
+
+/**
+ * Configure update for all a property's
+ * completed inspections
+ * @param  {String} propertyId
+ * @param  {Object[]} inspections
+ * @param  {Object} updates
+ * @return {Object} - configuration
+ */
+function updateNumOfInspections(config = { propertyId: '', inspections: [], updates: {} }) {
+  config.updates[`/properties/${config.propertyId}/numOfInspections`] = config.inspections.reduce(
+    (acc = 0, { inspectionCompleted }) => {
+      if (inspectionCompleted) {
+        acc += 1;
+      }
+
+      return acc;
+  }, 0);
+
+  return config;
+}
+
+/**
+ * Configure update for a property's
+ * latest inspection attributes
+ * @param  {String} propertyId
+ * @param  {Object[]} inspections
+ * @param  {Object} updates
+ * @return {Object} - configuration
+ */
+function updateLastInspectionAttrs(config = { propertyId: '', inspections: [], updates: {} }) {
+  const [latestInspection] = config.inspections.sort((a, b) => b.creationDate - a.creationDate); // DESC
+
+  if (latestInspection && latestInspection.inspectionCompleted) {
+    config.updates[`/properties/${config.propertyId}/lastInspectionScore`] = latestInspection.score;
+    config.updates[`/properties/${config.propertyId}/lastInspectionDate`] = latestInspection.creationDate;
+  }
+
+  return config;
 }
