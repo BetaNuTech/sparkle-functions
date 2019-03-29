b1f2549b715f70bca289cabec6fd279488b859b6
Matthew Jensen <matt@jwc.works>
2019-03-29 11:50:07 -0500

    #80 replace co with async/await in process-meta utility

--- functions/properties/process-meta.js
+++ functions/properties/process-meta.js
@@ -1,10 +1,9 @@
-const co = require('co');
 const log = require('../utils/logger');
 
 const LOG_PREFIX = 'inspections: process-property-meta:';
 
 /**
- * Process changes to a property's meta properties when it's
+ * Process changes to a property's metadata when it's
  * completed inspections' chanages
  * @param  {firebaseAdmin.database} db - Firebase Admin DB instance
  * @param  {String} propertyId
@@ -13,64 +12,62 @@ const LOG_PREFIX = 'inspections: process-property-meta:';
 module.exports = async function processMeta(db, propertyId) {
   const updates = Object.create(null);
 
-  return co(function *() {
-    // Pull all inspections for the same property
-    const inspectionsSnapshot = yield db.ref('/inspections').orderByChild('property').equalTo(propertyId).once('value');
+  // Pull all inspections for the same property
+  const inspectionsSnapshot = await db.ref('/inspections').orderByChild('property').equalTo(propertyId).once('value');
 
-    if (!inspectionsSnapshot.exists()) {
-      return updates;
-    }
+  if (!inspectionsSnapshot.exists()) {
+    return updates;
+  }
 
-    try {
-      var latestInspection = null;
-      var latestInspectionTmp = null;
-      var numOfInspectionsCompleted = 0;
-      const inspections = [];
+  try {
+    var latestInspection = null;
+    var latestInspectionTmp = null;
+    var numOfInspectionsCompleted = 0;
+    const inspections = [];
 
-      if (inspectionsSnapshot.hasChildren()) {
-        inspectionsSnapshot.forEach((childSnapshot) => {
-          // key will be 'ada' the first time and 'alan' the second time
-          latestInspectionTmp = childSnapshot.val();
+    if (inspectionsSnapshot.hasChildren()) {
+      inspectionsSnapshot.forEach((childSnapshot) => {
+        // key will be 'ada' the first time and 'alan' the second time
+        latestInspectionTmp = childSnapshot.val();
 
-          // childData will be the actual contents of the child
-          if (latestInspectionTmp.inspectionCompleted) {
-            inspections.push(latestInspectionTmp);
-            numOfInspectionsCompleted++;
-          }
-        });
-
-        if (inspections.length) {
-          latestInspection = inspections.sort((a, b) => b.creationDate - a.creationDate)[0]; // DESC
-        }
-      } else {
-        latestInspectionTmp = inspectionsSnapshot.val();
-
+        // childData will be the actual contents of the child
         if (latestInspectionTmp.inspectionCompleted) {
-          latestInspection = latestInspectionTmp;
+          inspections.push(latestInspectionTmp);
           numOfInspectionsCompleted++;
         }
+      });
+
+      if (inspections.length) {
+        latestInspection = inspections.sort((a, b) => b.creationDate - a.creationDate)[0]; // DESC
       }
+    } else {
+      latestInspectionTmp = inspectionsSnapshot.val();
 
-      // Update `numOfInspections` for the property
-      yield db.ref(`/properties/${propertyId}/numOfInspections`).set(numOfInspectionsCompleted);
-      log.info(`${LOG_PREFIX} property ${propertyId}: numOfInspections updated`);
-      updates[`/properties/${propertyId}/numOfInspections`] = 'updated';
-
-      if (latestInspection) {
-        yield db.ref(`/properties/${propertyId}`).update({
-          lastInspectionScore: latestInspection.score,
-          lastInspectionDate: latestInspection.creationDate
-        });
-
-        log.info(`${LOG_PREFIX} property ${propertyId}: lastInspectionScore & lastInspectionDate updated`);
-        updates[`/properties/${propertyId}/lastInspectionScore`] = 'updated';
-        updates[`/properties/${propertyId}/lastInspectionDate`] = 'updated';
+      if (latestInspectionTmp.inspectionCompleted) {
+        latestInspection = latestInspectionTmp;
+        numOfInspectionsCompleted++;
       }
-    } catch(e) {
-      log.error(`${LOG_PREFIX} property ${propertyId}: Unable to access inspections ${e}`);
-      return null;
     }
 
-    return updates;
-  });
+    // Update `numOfInspections` for the property
+    await db.ref(`/properties/${propertyId}/numOfInspections`).set(numOfInspectionsCompleted);
+    log.info(`${LOG_PREFIX} property ${propertyId}: numOfInspections updated`);
+    updates[`/properties/${propertyId}/numOfInspections`] = 'updated';
+
+    if (latestInspection) {
+      await db.ref(`/properties/${propertyId}`).update({
+        lastInspectionScore: latestInspection.score,
+        lastInspectionDate: latestInspection.creationDate
+      });
+
+      log.info(`${LOG_PREFIX} property ${propertyId}: lastInspectionScore & lastInspectionDate updated`);
+      updates[`/properties/${propertyId}/lastInspectionScore`] = 'updated';
+      updates[`/properties/${propertyId}/lastInspectionDate`] = 'updated';
+    }
+  } catch(e) {
+    log.error(`${LOG_PREFIX} property ${propertyId}: Unable to access inspections ${e}`);
+    return null;
+  }
+
+  return updates;
 }
