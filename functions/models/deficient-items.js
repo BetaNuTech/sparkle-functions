69e1a1ca61b9a7ca56cc64df7a274b6dfd1835d7
Matthew Jensen <matt@jwc.works>
2019-04-09 16:49:19 -0500

    #109 add deficient items model

---
+++ functions/models/deficient-items.js
@@ -0,0 +1,49 @@
+const assert = require('assert');
+const modelSetup = require('./utils/model-setup');
+
+const API_PATH = '/propertyInspectionDeficientItems';
+
+module.exports = modelSetup({
+  /**
+   * Find all DI's associated with a property
+   * @param  {firebaseAdmin.database} db - Firebase Admin DB instance
+   * @param  {String} inspectionId
+   * @return {Promise} - resolves {InspectionSnapshots[]}
+   */
+  async findAllByInspection(db, inspectionId) {
+    assert(inspectionId && typeof inspectionId === 'string', 'has inspection id');
+
+    const result = [];
+    const deficientItemsByPropertySnap = await db.ref(API_PATH).once('value');
+
+    // Add each DI belonging to an inspection to result
+    deficientItemsByPropertySnap.forEach(propertyDeficientItemsSnap => {
+      propertyDeficientItemsSnap.forEach(deficientItemsSnap => {
+        try {
+          if (deficientItemsSnap.val().inspection === inspectionId) {
+            result.push(deficientItemsSnap);
+          }
+        } catch (e) {}
+      });
+    });
+
+    return result;
+  },
+
+  /**
+   * Push deficient item into property path
+   * @param  {firebaseAdmin.database} db - Firebase Admin DB instance
+   * @param  {String} propertyId
+   * @param  {String} itemId
+   * @param  {Object} recordData
+   * @return {Promise} - resolves {??}
+   */
+  async createRecord(db, propertyId, itemId, recordData) {
+    assert(propertyId && typeof propertyId === 'string', 'has property id');
+    assert(itemId && typeof itemId === 'string', 'has item id');
+    assert(recordData && typeof recordData === 'object', 'has record date');
+    const path = `${API_PATH}/${propertyId}/${itemId}`;
+    await db.ref(path).set(recordData);
+    return { [path]: recordData };
+  }
+});
---
+++ functions/models/utils/model-setup.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+/**
+ * Assert that all model methods
+ * return a firebase admin database reference
+ * @param  {Object} target
+ * @return {Proxy} - proxy to db handlers
+ */
+module.exports = target => {
+  return new Proxy(target, {
+    apply(target, thisArg, args) {
+      assert(Boolean(args[0]), 'has firebase admin database reference');
+      return target.apply(thisArg, args);
+    }
+  });
+}
