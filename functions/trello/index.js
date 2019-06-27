b156f1549f441c020ee4bdb5e086bc12944ca27f
Matthew Jensen <matt@jwc.works>
2019-06-27 14:42:39 -0500

    #131 add GET endpoint of all a trello boards lists

--- functions/trello/index.js
+++ functions/trello/index.js
@@ -1,7 +1,9 @@
 const createOnUpsertTrelloTokenHandler = require('./on-create-request-handler');
 const createOnGetAllTrelloBoardsHandler = require('./get-all-trello-boards-handler');
+const createOnGetAllTrelloBoardListsHandler = require('./get-all-trello-board-lists-handler');
 
 module.exports = {
   createOnUpsertTrelloTokenHandler,
   createOnGetAllTrelloBoardsHandler,
+  createOnGetAllTrelloBoardListsHandler,
 };
