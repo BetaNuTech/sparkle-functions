6a7c3a72b65d9a4bae23dfe992eacbcb941656c4
Matthew Jensen <matt@jwc.works>
2019-07-15 15:15:16 -0500

    #170 use any slack secret from system, fallback to firebase env

--- scripts/deploy-functions.sh
+++ scripts/deploy-functions.sh
@@ -1,4 +1,5 @@
 FIREBASE_DEPLOY_FUNCTIONS="$(node ./scripts/functions-list-methods.js $1)" # Pass first arguement to script
 
 firebase functions:config:set auth.firebase=${FIREBASE_FUNCTIONS_AUTH}
+firebase functions:config:set slack.secret=${SLACK_CLIENT_SECRET}
 firebase deploy --only ${FIREBASE_DEPLOY_FUNCTIONS} --non-interactive --token $FIREBASE_TOKEN
