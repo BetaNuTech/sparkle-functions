7dc68e4fde07feb25925661c4f5b4e672c40e2a4
Matthew Jensen <matt@jwc.works>
2019-07-15 15:15:16 -0500

    #170 use any slack secret from system, fallback to firebase env

--- functions/config/slack-app.js
+++ functions/config/slack-app.js
@@ -1,7 +1,16 @@
-const { SLACK_CLIENT_SECRET } = process.env;
+const functions = require('firebase-functions');
+
+const firebaseConfig = functions.config() || {};
+
+// Use any system or firebase Slack secret
+const SLACK_CLIENT_SECRET =
+  process.env.SLACK_CLIENT_SECRET ||
+  (firebaseConfig.slack && firebaseConfig.slack.secret);
 
 if (!SLACK_CLIENT_SECRET) {
-  throw Error('SLACK_CLIENT_SECRET not set in Environment');
+  throw Error(
+    '"SLACK_CLIENT_SECRET" not set in system or Firebase Environment'
+  );
 }
 
 module.exports = {
