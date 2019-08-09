c7cb4c5538491f547d54ee46b870e5364fecb6ca
Matthew Jensen <matt@jwc.works>
2019-08-09 13:16:53 -0500

    #181 add removal of trello orgs credentials

--- functions/test/end-to-end/trello-delete-authorization.test.js
+++ functions/test/end-to-end/trello-delete-authorization.test.js
@@ -5,25 +5,25 @@ const uuid = require('../../test-helpers/uuid');
 const { cleanDb, stubFirbaseAuth } = require('../../test-helpers/firebase');
 const { db, uid: SERVICE_ACCOUNT_ID } = require('./setup');
 
-// const USER_ID = uuid();
+const USER_ID = uuid();
 // const PROPERTY_ID = uuid();
-// const USER_DATA = {
-//   firstName: 'mr',
-//   lastName: 'testor',
-//   email: 'test@gmail.com',
-//   admin: true,
-//   corporate: false,
-// };
+const USER_DATA = {
+  firstName: 'mr',
+  lastName: 'testor',
+  email: 'test@gmail.com',
+  admin: true,
+  corporate: false,
+};
 const API_PATH = '/integrations/trello/authorization';
-// const TRELLO_CREDENTIAL_DB_PATH = `/system/integrations/${SERVICE_ACCOUNT_ID}/trello/organization`;
+const TRELLO_CREDENTIAL_DB_PATH = `/system/integrations/${SERVICE_ACCOUNT_ID}/trello/organization`;
 // const TRELLO_INTEGRATIONS_DB_PATH = `/integrations/trello/properties/${PROPERTY_ID}`;
-// const TRELLO_CREDENTIALS_DATA = {
-//   member: 'akdf2334fasd',
-//   trelloUsername: 'jakew4',
-//   authToken: '1234dfasqruolfkj',
-//   apikey: '1234dlkfjasdlf',
-//   user: USER_ID,
-// };
+const TRELLO_CREDENTIALS_DATA = {
+  member: 'akdf2334fasd',
+  trelloUsername: 'jakew4',
+  authToken: '1234dfasqruolfkj',
+  apikey: '1234dlkfjasdlf',
+  user: USER_ID,
+};
 
 describe('Trello Delete Authorization', () => {
   afterEach(async () => {
@@ -31,7 +31,7 @@ describe('Trello Delete Authorization', () => {
     return db.ref(`/system/integrations/${SERVICE_ACCOUNT_ID}`).remove();
   });
 
-  it('should reject non-admin requester with an unauthorized response', async function() {
+  it('should reject non-admin requester with an unauthorized response', async () => {
     const unauthUserId = uuid();
 
     // Setup database
@@ -51,4 +51,28 @@ describe('Trello Delete Authorization', () => {
     // Assertions
     expect(result.body.message).to.equal('invalid credentials');
   });
+
+  it('should remove trello credentials for the organization', async () => {
+    // Setup database
+    await db.ref(`/users/${USER_ID}`).set(USER_DATA);
+    await db.ref(TRELLO_CREDENTIAL_DB_PATH).set(TRELLO_CREDENTIALS_DATA);
+
+    // Execute
+    const app = deleteTrelloAuthApp(db, stubFirbaseAuth(USER_ID));
+    await request(app)
+      .delete(API_PATH)
+      .set('Accept', 'application/json')
+      .set('Authorization', 'fb-jwt stubbed-by-auth')
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    // Result
+    const result = await db.ref(TRELLO_CREDENTIAL_DB_PATH).once('value');
+    const actual = result.exists();
+
+    // Assertions
+    expect(actual).to.equal(false);
+  });
+
+  // it('should allow any admin to remove the trello credentials of an organization', async () => { });
 });
