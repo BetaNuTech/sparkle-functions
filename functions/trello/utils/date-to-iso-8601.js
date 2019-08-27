21fa6c68cf415f1396bea71ef3db03629c13b03f
Matthew Jensen <matt@jwc.works>
2019-08-27 11:32:18 -0500

    #218 force EST offset for trello iso 8601 date strings

--- functions/trello/utils/date-to-iso-8601.js
+++ functions/trello/utils/date-to-iso-8601.js
@@ -2,11 +2,14 @@ const moment = require('moment');
 
 /**
  * Convert a `MM-DD-YYYY` date string to
- * an ISO 8601 formatted timestamp
+ * an ISO 8601 formatted timestamp at midnight
+ * in UTC offset
  * @param  {String} dateStr
  * @return {String}
  */
-module.exports = dateStr => {
+module.exports = (dateStr, utcOffset = '-05:00') => {
   const [month, date, year] = dateStr.split('/').map(str => parseInt(str, 10));
-  return moment([year, month - 1, date]).toISOString();
+  return moment([year, month - 1, date, 0, 0, 0])
+    .utcOffset(utcOffset, true)
+    .toISOString();
 };
