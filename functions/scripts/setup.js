23f75319b26e07e499561092d67c131f9d55359c
Matthew Jensen <matt@jwc.works>
2019-08-28 17:09:49 -0500

    #226 hack trello due dates to expire an UTC midnight which will be evening time in trello clients used EST

---
+++ functions/scripts/setup.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const dotenv = require('dotenv');
+
+try {
+  const cwd = process.cwd();
+
+  // Consume any `.env` in cwd and/or 2 parent directories
+  dotenv.config();
+  dotenv.config({ path: path.resolve(`${cwd}/..`, '.env') });
+  dotenv.config({ path: path.resolve(`${cwd}/../..`, '.env') });
+} catch (err) {} // eslint-disable-line no-empty
+
+const config = require('../config'); // eslint-disable-line
+const { firebase: fbConfig } = config;
+const admin = require('firebase-admin'); // eslint-disable-line
+const defaultApp = admin.initializeApp(fbConfig, 'script');
+const db = defaultApp.database(fbConfig.stagingDatabaseURL);
+const test = require('firebase-functions-test')(fbConfig); // eslint-disable-line
+const cloudFunctions = require('../index');
+
+module.exports = {
+  admin,
+  db,
+  test,
+  cloudFunctions,
+  config,
+};
