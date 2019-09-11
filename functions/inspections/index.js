e3299255dd1d234796a3655a4070b5e6e8bdc881
Matthew Jensen <matt@jwc.works>
2019-09-11 10:45:48 -0500

    #247 refactor inspections watchers

--- functions/inspections/index.js
+++ functions/inspections/index.js
@@ -1,9 +1,9 @@
 const cron = require('./cron');
 const processWrite = require('./process-write');
 const removeForProperty = require('./utils/remove-for-property');
-const createOnAttributeWriteHandler = require('./on-attribute-write-handler');
-const createOnWriteHandler = require('./on-write-handler');
-const createOnDeleteHandler = require('./on-delete-handler');
+const createOnAttributeWriteHandler = require('./on-attribute-write-watcher');
+const createOnWriteHandler = require('./on-write-watcher');
+const createOnDeleteHandler = require('./on-delete-watcher');
 const createOnGetPDFReportHandler = require('./on-get-pdf-report');
 const getLatestCompleted = require('./get-latest-completed');
 
