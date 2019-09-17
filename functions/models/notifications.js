5b47ae172c007bbc389adf2e18441e09a8904699
Matthew Jensen <matt@jwc.works>
2019-09-17 15:05:45 -0500

    #238 add publishing of slack notifications from source

--- functions/models/notifications.js
+++ functions/models/notifications.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const modelSetup = require('./utils/model-setup');
 
 const PREFIX = 'models: notifications:';
+const SRC_NOTIFICATION_PATH = '/notifications/src';
 const SLACK_NOTIFICATION_PATH = '/notifications/slack';
 
 module.exports = modelSetup({
@@ -9,6 +10,7 @@ module.exports = modelSetup({
    * Push a Slack notification to a
    * specified channel name resolving
    * DB reference to the new notification
+   * TODO: Deprecate
    * @param  {firebaseAdmin.database} db firbase database
    * @param  {String} channelName
    * @param  {Object} notification
@@ -35,5 +37,68 @@ module.exports = modelSetup({
     }
 
     return notificationRef;
+  },
+
+  /**
+   * Add a slack notification to a notification channel
+   * @param  {firebaseAdmin.database} db firbase database
+   * @param  {String} channelName
+   * @param  {String} notificationId
+   * @param  {Object} notification
+   * @return {Promise} - resolves {db.Ref}
+   */
+  addToSlackChannel(db, channelName, notificationId, notification = {}) {
+    assert(
+      channelName && typeof channelName === 'string',
+      `${PREFIX} addToSlackChannel: has Slack channel name`
+    );
+    assert(
+      notificationId && typeof notificationId === 'string',
+      `${PREFIX} addToSlackChannel: has notification ID`
+    );
+    assert(
+      notification && notification.title && notification.message,
+      `${PREFIX} addToSlackChannel: has valid notification`
+    );
+
+    const ref = db.ref(
+      `${SLACK_NOTIFICATION_PATH}/${channelName}/${notificationId}`
+    );
+
+    return ref
+      .set(notification)
+      .then(() => ref)
+      .catch(err =>
+        Promise.reject(
+          Error(`${PREFIX} addToSlackChannel | ${err}`) // wrap error
+        )
+      );
+  },
+
+  /**
+   * Update a source notification's published mediums
+   * @param  {firebaseAdmin.database} db firbase database
+   * @param  {String} notificationId
+   * @param  {Object} update
+   * @return {Promise}
+   */
+  updateSrcPublishedMediums(db, notificationId, update) {
+    assert(
+      notificationId && typeof notificationId === 'string',
+      `${PREFIX} updateSrcPublishedMediums: has notification ID`
+    );
+    assert(
+      update && typeof update === 'object',
+      `${PREFIX} updateSrcPublishedMediums: has published medium updates`
+    );
+
+    return db
+      .ref(`${SRC_NOTIFICATION_PATH}/${notificationId}/publishedMediums`)
+      .update(update)
+      .catch(err =>
+        Promise.reject(
+          Error(`${PREFIX} updateSrcPublishedMediums | ${err}`) // wrap error
+        )
+      );
   },
 });
--- functions/notifications/on-create-src-slack-watcher.js
+++ functions/notifications/on-create-src-slack-watcher.js
@@ -1,7 +1,10 @@
 const assert = require('assert');
 const log = require('../utils/logger');
+const propertiesModel = require('../models/properties');
+const integrationsModel = require('../models/integrations');
+const notificationsModel = require('../models/notifications');
 
-const PREFIX = 'notifications: on-create-src-watcher:';
+const PREFIX = 'notifications: on-create-src-slack-watcher:';
 
 /**
  * Factory for watcher to create Slack notifications
@@ -9,10 +12,23 @@ const PREFIX = 'notifications: on-create-src-watcher:';
  * NOTE: source notications `/notifications/src/*`
  * NOTE: slack notications `/notifications/slack/channel/*`
  * @param  {firebaseAdmin.database} database - Firebase Admin DB instance
+ * @param  {functions.pubsub} pubsubClient
+ * @param  {String} notificationsSyncTopic
  * @return {Function} - notifications source onCreate handler
  */
-module.exports = function createOnCreateSrcSlackNotification(db) {
+module.exports = function createOnCreateSrcSlackNotification(
+  db,
+  pubsubClient,
+  notificationsSyncTopic
+) {
   assert(Boolean(db), 'has firebase admin database reference');
+  assert(Boolean(pubsubClient), 'has pubsub client');
+  assert(
+    notificationsSyncTopic && typeof notificationsSyncTopic === 'string',
+    'has notification topic'
+  );
+
+  const publisher = pubsubClient.topic(notificationsSyncTopic).publisher();
 
   return async (change, event) => {
     const { notificationId } = event.params;
@@ -20,13 +36,20 @@ module.exports = function createOnCreateSrcSlackNotification(db) {
     const {
       title,
       summary,
-      // property: proeprtyId,
-      // markdownBody,
+      property: propertyId,
+      markdownBody,
+      publishedMediums,
     } = srcNotification;
 
     assert(Boolean(notificationId), 'has deficient item ID');
     assert(Boolean(srcNotification), 'has source notification');
-    log.info(`${PREFIX} source notification "${notificationId}" generated`);
+
+    // Slack notification previously configured
+    if (publishedMediums && publishedMediums.slack) {
+      return;
+    }
+
+    log.info(`${PREFIX} source notification "${notificationId}" discovered`);
 
     // Reject invalid source notification
     if (!title) {
@@ -36,5 +59,77 @@ module.exports = function createOnCreateSrcSlackNotification(db) {
         `${PREFIX} source notification has invalid summary: ${summary}`
       );
     }
+
+    let channelName = '';
+
+    // Property notification
+    if (propertyId) {
+      let property = null;
+      try {
+        const propertySnap = await propertiesModel.findRecord(db, propertyId);
+        property = propertySnap.val();
+        if (!property) throw Error('property does not exist');
+      } catch (err) {
+        throw Error(`${PREFIX} property lookup failed | ${err}`); // wrap error
+      }
+
+      channelName = property.slackChannel;
+    }
+
+    // Admin notification
+    if (!propertyId) {
+      try {
+        const slackOrgSnap = await integrationsModel.getSlackOrganization(db);
+        const adminChannelName = (slackOrgSnap.val() || {}).defaultChannelName;
+        if (adminChannelName) channelName = adminChannelName;
+      } catch (err) {
+        throw Error(
+          `${PREFIX} organization slack channel integration lookup failed | ${err}`
+        ); // wrap error
+      }
+    }
+
+    // Abandon when channel undiscovered
+    if (!channelName) {
+      const message = propertyId
+        ? 'No Slack channel associated with this property'
+        : 'Admin channel has not been setup';
+      throw Error(`${PREFIX} ${message}`);
+    }
+
+    // Ensure channel `#` removed
+    channelName = channelName.replace(/#/g, '');
+
+    // Create notification record
+    // and queue notification sync task
+    try {
+      const notificationRef = await notificationsModel.addToSlackChannel(
+        db,
+        channelName,
+        notificationId,
+        { title, message: markdownBody || summary }
+      );
+
+      log.info(
+        `${PREFIX} created slack notification: ${notificationRef.path.toString()}`
+      );
+    } catch (err) {
+      throw Error(
+        `${PREFIX} failed to create and/or publish Slack notification | ${err}`
+      );
+    }
+
+    // Update the source notifications publised mediums
+    try {
+      await notificationsModel.updateSrcPublishedMediums(db, notificationId, {
+        slack: true,
+      });
+    } catch (err) {
+      log.error(`${PREFIX} failed to mark published mediums | ${err}`);
+    }
+
+    // Publish notification sync
+    // event for the Slack channel
+    await publisher.publish(Buffer.from(`slack/${channelName}`));
   };
 };
