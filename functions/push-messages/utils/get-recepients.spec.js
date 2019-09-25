6b7bc6122cbf3e6a0c253e1f15ac5ddcfd8bb26c
Matthew Jensen <matt@jwc.works>
2019-09-25 16:58:20 -0500

    #265 add opt out users to push notification excludes users

---
+++ functions/push-messages/utils/get-recepients.spec.js
@@ -0,0 +1,49 @@
+const { expect } = require('chai');
+const uuid = require('../../test-helpers/uuid');
+const getRecepients = require('./get-recepients');
+
+const PROPERTY_ID = uuid();
+const USERS = {
+  admin: {
+    id: uuid(),
+    admin: true,
+  },
+  corporate: {
+    id: uuid(),
+    corporate: true,
+  },
+  property: {
+    id: uuid(),
+    properties: { [PROPERTY_ID]: true },
+  },
+};
+
+describe('Push Messages | Utils | Get Recepients', () => {
+  it('should add all admin users as recepients', () => {
+    const users = [USERS.admin, USERS.corporate, USERS.property];
+    const expected = [USERS.admin.id];
+    const actual = getRecepients({ users });
+    expect(actual).to.deep.equal(expected);
+  });
+
+  it('should add all corporate users when permitted', () => {
+    const users = [USERS.admin, USERS.corporate, USERS.property];
+    const expected = [USERS.admin.id, USERS.corporate.id];
+    const actual = getRecepients({ users, allowCorp: true });
+    expect(actual).to.deep.equal(expected);
+  });
+
+  it('should add all property level users matching a property', () => {
+    const users = [USERS.admin, USERS.corporate, USERS.property];
+    const expected = [USERS.admin.id, USERS.property.id];
+    const actual = getRecepients({ users, property: PROPERTY_ID });
+    expect(actual).to.deep.equal(expected);
+  });
+
+  it('should remove all users in excluded', () => {
+    const users = [USERS.admin, USERS.corporate, USERS.property];
+    const expected = [];
+    const actual = getRecepients({ users, excludes: [USERS.admin.id] });
+    expect(actual).to.deep.equal(expected);
+  });
+});
