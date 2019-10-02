59a4398961e053e435cb7b049d570151950de912
Matthew Jensen <matt@jwc.works>
2019-10-02 17:46:39 -0500

    #267 a on create src notification watcher

--- functions/notifications/index.js
+++ functions/notifications/index.js
@@ -1,9 +1,11 @@
 const createOnCreateSrcSlackWatcher = require('./on-create-src-slack-watcher');
+const createOnCreateSrcPushWatcher = require('./on-create-src-push-watcher');
 const createCleanup = require('./pubsub/cleanup');
 const createPublishSlack = require('./pubsub/publish-slack');
 
 module.exports = {
   createOnCreateSrcSlackWatcher,
+  createOnCreateSrcPushWatcher,
 
   pubsub: {
     createCleanup,
