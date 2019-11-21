c05602a79676fc4b2d65a3da603e7750a5eb09dc
Matthew Jensen <matt@jwc.works>
2019-11-21 17:00:15 -0600

    #285 add inspection PDF GET endpoint to new API conventions

--- functions/router.js
+++ functions/router.js
@@ -10,13 +10,15 @@ const authUser = require('./utils/auth-firebase-user');
  * all API endpoints
  * @param  {firebaseAdmin.database} db - Firebase Admin DB instance
  * @param  {firebaseAdmin.auth} auth - Firebase Admin auth instance
+ * @param  {Object} settings
  * @return {Express}
  */
-module.exports = (db, auth) => {
+module.exports = (db, auth, settings) => {
   assert(Boolean(db), 'has firebase database instance');
   assert(Boolean(auth), 'has firebase auth instance');
 
   const app = express();
+  const { inspectionUrl } = settings;
 
   // Inspection property
   // reassignment endpoint
@@ -26,6 +28,14 @@ module.exports = (db, auth) => {
     bodyParser.json(),
     authUser(db, auth, true), // admin only
     inspections.api.createPatchProperty(db)
+  );
+
+  // Generate Inspection PDF report
+  app.get(
+    '/v0/inspections/:inspection/pdf-report',
+    cors(),
+    authUser(db, auth),
+    inspections.api.createGetInspectionPDF(db, inspectionUrl)
   );
 
   return app;
