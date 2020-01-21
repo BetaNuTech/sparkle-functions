11797a1b4a5d866e00521e8a32e665ddf434c2a1
Matthew Jensen <matt@jwc.works>
2020-01-21 17:45:35 -0600

    #322 set max parallel to 1

--- functions/scripts/clean-test-db.js
+++
@@ -1,9 +0,0 @@
-const { db } = require('../test/end-to-end/setup'); // eslint-disable-line
-const { cleanDb } = require('../test-helpers/firebase');
-
-(async () => {
-  await cleanDb(db);
-
-  // End
-  process.exit();
-})();
