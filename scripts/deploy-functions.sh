94d2886c4886318561806ae0065e50c4eaa9c825
Matthew Jensen <matt@jwc.works>
2020-03-05 17:55:01 -0600

    #330 hard code only inspection PDF endpoint

--- scripts/deploy-functions.sh
+++ scripts/deploy-functions.sh
@@ -2,4 +2,4 @@ FIREBASE_DEPLOY_FUNCTIONS="$(node ./scripts/functions-list-methods.js $1)" # Pas
 
 firebase functions:config:set auth.firebase=${FIREBASE_FUNCTIONS_AUTH}
 firebase functions:config:set slack.secret=${SLACK_CLIENT_SECRET}
-firebase deploy --only ${FIREBASE_DEPLOY_FUNCTIONS} --non-interactive --token $FIREBASE_TOKEN
+firebase deploy --only functions:inspectionPdfReport,functions:api --non-interactive --token $FIREBASE_TOKEN
