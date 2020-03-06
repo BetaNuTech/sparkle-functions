def1671a2c6b56bee2a2adab68de828d2ef55367
Matthew Jensen <matt@jwc.works>
2020-03-05 18:36:08 -0600

    #330 configure AWS from firebase config

--- scripts/deploy-functions.sh
+++ scripts/deploy-functions.sh
@@ -1,6 +1,7 @@
 FIREBASE_DEPLOY_FUNCTIONS="$(node ./scripts/functions-list-methods.js $1)" # Pass first arguement to script
 
-echo "$AWS_S3_SECRET_ACCESS_ID"
 firebase functions:config:set auth.firebase=${FIREBASE_FUNCTIONS_AUTH}
 firebase functions:config:set slack.secret=${SLACK_CLIENT_SECRET}
+firebase functions:config:set aws.id=${AWS_S3_ACCESS_KEY_ID}
+firebase functions:config:set aws.key=${AWS_S3_SECRET_ACCESS_KEY}
 firebase deploy --only functions:inspectionPdfReport,functions:api --non-interactive --token $FIREBASE_TOKEN
