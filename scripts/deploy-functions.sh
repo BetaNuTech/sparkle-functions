7f94ff024ef92aab34dbe7f826fc31f94e70b7ee
Matthew Jensen <matt@jwc.works>
2020-03-17 15:36:50 -0500

    #338 configure client domain via firebase config

--- scripts/deploy-functions.sh
+++ scripts/deploy-functions.sh
@@ -4,4 +4,5 @@ firebase functions:config:set auth.firebase=${FIREBASE_FUNCTIONS_AUTH}
 firebase functions:config:set slack.secret=${SLACK_CLIENT_SECRET}
 firebase functions:config:set aws.id=${AWS_S3_ACCESS_KEY_ID}
 firebase functions:config:set aws.key=${AWS_S3_SECRET_ACCESS_KEY}
+firebase functions:config:set web.clientDomain=${CLIENT_DOMAIN}
 firebase deploy --only $FIREBASE_DEPLOY_FUNCTIONS --non-interactive --token $FIREBASE_TOKEN
