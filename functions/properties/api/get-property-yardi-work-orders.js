30751ec115a34df787bfeb58f58796b713495d32
Matthew Jensen <matt@jwc.works>
2020-04-21 16:54:31 -0500

    #363 add yardi work order API endpoint JSON API response

--- functions/properties/api/get-property-yardi-work-orders.js
+++ functions/properties/api/get-property-yardi-work-orders.js
@@ -1,11 +1,14 @@
 const assert = require('assert');
+const log = require('../../utils/logger');
+const yardi = require('../../services/yardi');
+const create500ErrHandler = require('../../utils/unexpected-api-error');
 
-// const PREFIX = 'properties: api: get-property-yardi-work-orders:';
+const PREFIX = 'properties: api: get-property-yardi-work-orders:';
 
 /**
  * Factory for creating a GET endpoint
  * that fetches a properties Yardi Work Orders
- * @param {admin.database} db
+ * @param {admin.firestore} db
  * @return {Function} - onRequest handler
  */
 module.exports = function createGetYardiResidents(db) {
@@ -17,8 +20,83 @@ module.exports = function createGetYardiResidents(db) {
    * @param  {Object} res Express res
    * @return {Promise}
    */
-  return async (req /* , res */) => {
+  return async (req, res) => {
     assert(req.property, 'has property set by middleware');
-    // const property = req.property;
+    assert(req.yardiConfig, 'has yardi config set by middleware');
+    const property = req.property;
+    const yardiConfig = req.yardiConfig;
+    const send500Error = create500ErrHandler(PREFIX, res);
+
+    let workOrders = null;
+
+    // Lookup Work Orders
+    try {
+      const result = await yardi.getYardiPropertyWorkOrders(
+        property.code,
+        yardiConfig
+      );
+      workOrders = result.workOrders;
+    } catch (err) {
+      if (err.code) log.error(`${PREFIX} | ${err}`);
+
+      // Bad property code
+      if (err.code === 'ERR_NO_YARDI_PROPERTY') {
+        return res.status(404).send({
+          errors: [
+            {
+              detail: 'Configured yardi code for property returned no results',
+              source: { pointer: 'code' },
+            },
+          ],
+        });
+      }
+
+      // Bad system integration credentials
+      if (err.code === 'ERR_BAD_YARDI_CREDENTIALS') {
+        return res.status(401).send({
+          errors: [
+            {
+              detail: 'Yardi integration credentials not accepted',
+            },
+          ],
+        });
+      }
+
+      return send500Error(
+        err,
+        'Yard request failed',
+        'Unexpected error fetching work orders, please try again'
+      );
+    }
+
+    const json = { data: [] };
+
+    // Push all work orders to payload
+    // as JSON API formatted records
+    workOrders.forEach(workOrder => {
+      const result = {
+        id: workOrder.id,
+        type: 'work-order',
+        attributes: { ...workOrder },
+      };
+
+      if (workOrder.resident) {
+        result.relationships = {
+          resident: {
+            data: workOrder.resident,
+          },
+        };
+      }
+
+      // Remove ID and relationships
+      // from record attributes
+      delete result.attributes.id;
+      delete result.attributes.resident;
+
+      json.data.push(result);
+    });
+
+    // Success
+    res.status(200).send(json);
   };
 };
