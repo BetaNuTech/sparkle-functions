0a1f1a455a76bb457b14a21c54b3fe80145cb720
Matthew Jensen <matt@jwc.works>
2020-05-13 15:41:00 -0500

    #398 add documentation around DI property meta updates

--- functions/scripts/sync-firestore-properties.js
+++ functions/scripts/sync-firestore-properties.js
@@ -3,15 +3,23 @@ const { db, fs } = require('./setup'); // eslint-disable-line
 const utils = require('../utils/firebase-admin');
 const propertiesModel = require('../models/properties');
 const templatesModel = require('../models/templates');
+const properties = require('../properties');
 
 (async () => {
   await utils.forEachChild(db, '/properties', async (id, data) => {
+    if (id !== '-LzNJq6Qj5QfodyxlWxy') return; // TODO: remove
     log.info(`Syning property "${id}"`);
 
     try {
       await propertiesModel.firestoreUpsertRecord(fs, id, data);
     } catch (err) {
       log.error(`Failed to sync property "${id}" to Firestore | ${err}`);
+    }
+
+    try {
+      await properties.utils.processMeta(db, fs, id);
+    } catch (err) {
+      log.error(`Failed to sync property "${id}" meta data | ${err}`);
     }
 
     try {
