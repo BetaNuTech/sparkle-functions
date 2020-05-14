8e3cb9b2c3eec68e7c7b92f3c845398109f1b75b
Matthew Jensen <matt@jwc.works>
2020-05-14 15:45:54 -0500

    #392 add syncing script

---
+++ functions/scripts/sync-firestore-template-categories.js
@@ -0,0 +1,19 @@
+const log = require('../utils/logger');
+const { db, fs } = require('./setup'); // eslint-disable-line
+const utils = require('../utils/firebase-admin');
+const templateCategoriesModel = require('../models/template-categories');
+
+(async () => {
+  await utils.forEachChild(db, '/templateCategories', async (id, data) => {
+    log.info(`Syning template category "${id}"`);
+
+    try {
+      await templateCategoriesModel.firestoreUpsertRecord(fs, id, data);
+    } catch (err) {
+      log.error(`Failed to sync template category "${id}" to Firestore`);
+    }
+  });
+
+  log.info('Completed template category sync successfully');
+  process.exit();
+})();
