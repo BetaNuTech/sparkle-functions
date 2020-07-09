916bd03d82292450eaf6fcc657b86358034b7c07
Matthew Jensen <matt@jwc.works>
2020-07-09 09:51:56 -0500

    #436 add tests to new deficiency progress note update handler

--- functions/test/end-to-end/deficient-items/update-progress-notes-v2.test.js
+++ functions/test/end-to-end/deficient-items/update-progress-notes-v2.test.js
@@ -10,43 +10,7 @@ const { cleanDb } = require('../../../test-helpers/firebase');
 const userModel = require('../../../models/users');
 const deficiencyModel = require('../../../models/deficient-items');
 const systemModel = require('../../../models/system');
-const deferredDiData = require('../../../test-helpers/mocks/deferred-deficient-item');
 const { fs, test, cloudFunctions } = require('../../setup');
-
-const USER_ID = uuid();
-const PROPERTY_ID = uuid();
-const DEFICIENT_ITEM_ID = uuid();
-const PROGRESS_NOTE_ID = uuid();
-const TRELLO_CARD_ID = uuid();
-const TRELLO_API_KEY = '2342afab';
-const TRELLO_AUTH_TOKEN = '909adfjsd';
-const TRELLO_CREDENTIAL_PATH = `/system/integrations/trello/organization`;
-const TRELLO_SYSTEM_INTEGRATION_DATA = {
-  user: uuid(),
-  apikey: TRELLO_API_KEY,
-  authToken: TRELLO_AUTH_TOKEN,
-};
-const TRELLO_CARDS_PATH = `/system/integrations/trello/properties/${PROPERTY_ID}/cards`;
-const TRELLO_CARD_DATA = { [TRELLO_CARD_ID]: DEFICIENT_ITEM_ID };
-const DEFICIENT_ITEM_PATH = `/propertyInspectionDeficientItems/${PROPERTY_ID}/${DEFICIENT_ITEM_ID}`;
-const PROGRESS_NOTE_PATH = `/propertyInspectionDeficientItems/${PROPERTY_ID}/${DEFICIENT_ITEM_ID}/progressNotes/${PROGRESS_NOTE_ID}`;
-const DEFICIENT_ITEM_DATA = JSON.parse(JSON.stringify(deferredDiData)); // deep clone
-DEFICIENT_ITEM_DATA.progressNotes = {
-  [PROGRESS_NOTE_ID]: {
-    startDate: Math.round(Date.now() / 1000),
-    createdAt: Math.round(Date.now() / 1000),
-    progressNote: 'test note',
-    user: USER_ID,
-  },
-};
-const USER_PATH = `/users/${USER_ID}`;
-const USER_DATA = {
-  admin: true,
-  corporate: false,
-  email: 'testor@bluestone-prop.com',
-  firstName: 'Test',
-  lastName: 'User',
-};
 
 describe('Deficiency | Update Progress Notes V2', () => {
   afterEach(async () => {
@@ -54,7 +18,8 @@ describe('Deficiency | Update Progress Notes V2', () => {
     await cleanDb(null, fs);
   });
 
-  it('should not publish comment when progress notes unchanged', async () => {
+  it("publishes a progress note comment to a Deficiency's Trello Card", async () => {
+    const expected = true;
     const cardId = uuid();
     const userId = uuid();
     const propertyId = uuid();
@@ -66,6 +31,7 @@ describe('Deficiency | Update Progress Notes V2', () => {
       property: propertyId,
       inspection: inspectionId,
       item: itemId,
+      trelloCardURL: `https://trello.com/cards/${cardId}`,
       progressNotes: {
         [progNoteId]: {
           startDate: '1/11/25',
@@ -75,6 +41,15 @@ describe('Deficiency | Update Progress Notes V2', () => {
         },
       },
     });
+    // Add new progress note
+    const defUpdate = {
+      [`progressNotes.${uuid()}`]: {
+        startDate: '1/11/25',
+        progressNote: 'text',
+        user: userId,
+        createdAt: now() + 1,
+      },
+    };
     const trelloCredentials = {
       authToken: 'token',
       apikey: 'key',
@@ -110,9 +85,7 @@ describe('Deficiency | Update Progress Notes V2', () => {
       fs,
       deficiencyId
     );
-    await deficiencyModel.firestoreUpdateRecord(fs, deficiencyId, {
-      updatedAt: now(),
-    });
+    await deficiencyModel.firestoreUpdateRecord(fs, deficiencyId, defUpdate);
     const afterSnap = await deficiencyModel.firestoreFindRecord(
       fs,
       deficiencyId
@@ -125,153 +98,128 @@ describe('Deficiency | Update Progress Notes V2', () => {
 
     // Assertions
     const actual = commentCreated.isDone();
-    expect(actual).to.equal(false);
+    expect(actual).to.equal(expected);
   });
 
-  // it('should not make request to Trello API when Progress Note lacks user', async () => {
-  //   // Stup requests
-  //   const commentCreated = nock('https://api.trello.com')
-  //     .post(
-  //       `/1/cards/${TRELLO_CARD_ID}/actions/comments?key=${TRELLO_API_KEY}&token=${TRELLO_AUTH_TOKEN}&text=test`
-  //     )
-  //     .reply(200, {});
-  //
-  //   // Setup database
-  //   const diData = JSON.parse(JSON.stringify(DEFICIENT_ITEM_DATA));
-  //   delete diData.progressNotes[PROGRESS_NOTE_ID].user;
-  //   await db.ref(DEFICIENT_ITEM_PATH).set(diData);
-  //   await db.ref(TRELLO_CREDENTIAL_PATH).set(TRELLO_SYSTEM_INTEGRATION_DATA);
-  //   await db.ref(TRELLO_CARDS_PATH).set(TRELLO_CARD_DATA);
-  //   await db.ref(USER_PATH).set(USER_DATA);
-  //   const changeSnap = await db.ref(PROGRESS_NOTE_PATH).once('value');
-  //
-  //   // Execute
-  //   try {
-  //     const wrapped = test.wrap(
-  //       cloudFunctions.onCreateDeficientItemProgressNoteTrelloComment
-  //     );
-  //     await wrapped(changeSnap, {
-  //       params: {
-  //         propertyId: PROPERTY_ID,
-  //         deficientItemId: DEFICIENT_ITEM_ID,
-  //         progressNoteId: PROGRESS_NOTE_ID,
-  //       },
-  //     });
-  //   } catch (err) {} // eslint-disable-line no-empty
-  //
-  //   // Assertions
-  //   const actual = commentCreated.isDone();
-  //   expect(actual).to.equal(false);
-  // });
-  //
-  // it('should not make request to Trello API when Deficient Item has no Trello Card', async () => {
-  //   // Stup requests
-  //   const commentCreated = nock('https://api.trello.com')
-  //     .post(
-  //       `/1/cards/${TRELLO_CARD_ID}/actions/comments?key=${TRELLO_API_KEY}&token=${TRELLO_AUTH_TOKEN}&text=test`
-  //     )
-  //     .reply(200, {});
-  //
-  //   // Setup database
-  //   await db.ref(USER_PATH).set(USER_DATA);
-  //   await db.ref(DEFICIENT_ITEM_PATH).set(DEFICIENT_ITEM_DATA);
-  //   await db.ref(TRELLO_CREDENTIAL_PATH).set(TRELLO_SYSTEM_INTEGRATION_DATA);
-  //   const changeSnap = await db.ref(PROGRESS_NOTE_PATH).once('value');
-  //
-  //   // Execute
-  //   try {
-  //     const wrapped = test.wrap(
-  //       cloudFunctions.onCreateDeficientItemProgressNoteTrelloComment
-  //     );
-  //     await wrapped(changeSnap, {
-  //       params: {
-  //         propertyId: PROPERTY_ID,
-  //         deficientItemId: DEFICIENT_ITEM_ID,
-  //         progressNoteId: PROGRESS_NOTE_ID,
-  //       },
-  //     });
-  //   } catch (err) {} // eslint-disable-line no-empty
-  //
-  //   // Assertions
-  //   const actual = commentCreated.isDone();
-  //   expect(actual).to.equal(false);
-  // });
-  //
-  // it('should not make request to Trello API when Progress Note author does not exist', async () => {
-  //   // Stup requests
-  //   const commentCreated = nock('https://api.trello.com')
-  //     .post(
-  //       `/1/cards/${TRELLO_CARD_ID}/actions/comments?key=${TRELLO_API_KEY}&token=${TRELLO_AUTH_TOKEN}&text=test`
-  //     )
-  //     .reply(200, {});
-  //
-  //   // Setup database
-  //   await db.ref(TRELLO_CARDS_PATH).set(TRELLO_CARD_DATA);
-  //   await db.ref(DEFICIENT_ITEM_PATH).set(DEFICIENT_ITEM_DATA);
-  //   await db.ref(TRELLO_CREDENTIAL_PATH).set(TRELLO_SYSTEM_INTEGRATION_DATA);
-  //   const changeSnap = await db.ref(PROGRESS_NOTE_PATH).once('value');
-  //
-  //   // Execute
-  //   try {
-  //     const wrapped = test.wrap(
-  //       cloudFunctions.onCreateDeficientItemProgressNoteTrelloComment
-  //     );
-  //     await wrapped(changeSnap, {
-  //       params: {
-  //         propertyId: PROPERTY_ID,
-  //         deficientItemId: DEFICIENT_ITEM_ID,
-  //         progressNoteId: PROGRESS_NOTE_ID,
-  //       },
-  //     });
-  //   } catch (err) {} // eslint-disable-line no-empty
-  //
-  //   // Assertions
-  //   const actual = commentCreated.isDone();
-  //   expect(actual).to.equal(false);
-  // });
-  //
-  // it("should publish a progress note comment to the Deficient Item's Trello Card", async () => {
-  //   // Stup requests
-  //   const comment = hbs.compile(trelloCardDIProgressNoteTemplate)({
-  //     firstName: USER_DATA.firstName,
-  //     lastName: USER_DATA.lastName,
-  //     email: USER_DATA.email,
-  //     progressNote:
-  //       DEFICIENT_ITEM_DATA.progressNotes[PROGRESS_NOTE_ID].progressNote,
-  //   });
-  //   const commentCreated = nock('https://api.trello.com')
-  //     .post(
-  //       `/1/cards/${TRELLO_CARD_ID}/actions/comments?key=${TRELLO_API_KEY}&token=${TRELLO_AUTH_TOKEN}&text=${encodeURIComponent(
-  //         comment
-  //       )}`
-  //     )
-  //     .reply(200, {});
-  //
-  //   // Setup database
-  //   await db.ref(USER_PATH).set(USER_DATA);
-  //   await db.ref(TRELLO_CARDS_PATH).set(TRELLO_CARD_DATA);
-  //   await db.ref(DEFICIENT_ITEM_PATH).set(DEFICIENT_ITEM_DATA);
-  //   await db.ref(TRELLO_CREDENTIAL_PATH).set(TRELLO_SYSTEM_INTEGRATION_DATA);
-  //   const changeSnap = await db.ref(PROGRESS_NOTE_PATH).once('value');
-  //
-  //   // Execute
-  //   try {
-  //     const wrapped = test.wrap(
-  //       cloudFunctions.onCreateDeficientItemProgressNoteTrelloComment
-  //     );
-  //     await wrapped(changeSnap, {
-  //       params: {
-  //         propertyId: PROPERTY_ID,
-  //         deficientItemId: DEFICIENT_ITEM_ID,
-  //         progressNoteId: PROGRESS_NOTE_ID,
-  //       },
-  //     });
-  //   } catch (err) {} // eslint-disable-line no-empty
-  //
-  //   // Assertions
-  //   const actual = commentCreated.isDone();
-  //   expect(actual).to.equal(true);
-  // });
+  it('should cleanup references to delete Trello card from database', async () => {
+    const cardId = uuid();
+    const userId = uuid();
+    const propertyId = uuid();
+    const itemId = uuid();
+    const inspectionId = uuid();
+    const deficiencyId = uuid();
+    const progNoteId = uuid();
+    const completedPhotoId = uuid();
+    const deficiencyData = mocking.createDeficiency({
+      property: propertyId,
+      inspection: inspectionId,
+      item: itemId,
+      trelloCardURL: `https://trello.com/cards/${cardId}`,
+      progressNotes: {
+        [progNoteId]: {
+          startDate: '1/11/25',
+          progressNote: 'text',
+          user: userId,
+          createdAt: now(),
+        },
+      },
+      completedPhotos: {
+        [completedPhotoId]: {
+          downloadURL: 'test.com',
+          trelloCardAttachement: 'exists',
+        },
+      },
+    });
+    // Add new progress note
+    const defUpdate = {
+      [`progressNotes.${uuid()}`]: {
+        startDate: '1/11/25',
+        progressNote: 'text',
+        user: userId,
+        createdAt: now() + 1,
+      },
+    };
+    const trelloCredentials = {
+      authToken: 'token',
+      apikey: 'key',
+      user: userId,
+    };
+    const trelloPropertyData = {
+      cards: { [cardId]: deficiencyId },
+    };
+    const userData = createUser();
+    const trelloComment = createTrelloCommentText(userData, 'text');
+
+    // Stup requests
+    nock('https://api.trello.com')
+      .post(
+        `/1/cards/${cardId}/actions/comments?key=key&token=token&text=${trelloComment}`
+      )
+      .reply(404, {});
+
+    // Setup database
+    await deficiencyModel.firestoreCreateRecord(
+      fs,
+      deficiencyId,
+      deficiencyData
+    );
+    await systemModel.firestoreUpsertTrello(fs, trelloCredentials);
+    await systemModel.firestoreCreateTrelloProperty(
+      fs,
+      propertyId,
+      trelloPropertyData
+    );
+    await userModel.firestoreCreateRecord(fs, userId, userData);
+    const beforeSnap = await deficiencyModel.firestoreFindRecord(
+      fs,
+      deficiencyId
+    );
+    await deficiencyModel.firestoreUpdateRecord(fs, deficiencyId, defUpdate);
+    const afterSnap = await deficiencyModel.firestoreFindRecord(
+      fs,
+      deficiencyId
+    );
+    const changeSnap = test.makeChange(beforeSnap, afterSnap);
+
+    // Execute
+    const wrapped = test.wrap(cloudFunctions.deficientItemsProgressNotesSyncV2);
+    await wrapped(changeSnap, { params: { deficiencyId } });
+
+    // Test Results
+    const trelloPropertySnap = await systemModel.firestoreFindTrelloProperty(
+      fs,
+      propertyId
+    );
+    const deficiencySnap = await deficiencyModel.firestoreFindRecord(
+      fs,
+      deficiencyId
+    );
+    const deficiency = deficiencySnap.data() || { trelloCardURL: 'test' };
+    const completedPhoto = (deficiency.completedPhotos || {})[
+      completedPhotoId
+    ] || { trelloCardAttachement: 'test' };
+
+    // Assertions
+    [
+      {
+        actual: ((trelloPropertySnap.data() || {}).cards || {})[cardId],
+        expected: undefined,
+        msg: 'Removed Trello card from systems property data',
+      },
+      {
+        actual: deficiency.trelloCardURL,
+        expected: undefined,
+        msg: 'Removed Trello card url from systems property data',
+      },
+      {
+        actual: completedPhoto.trelloCardAttachement,
+        expected: undefined,
+        msg: 'Removed Trello card attachemnt from completed photo',
+      },
+    ].forEach(({ actual, expected, msg }) => {
+      expect(actual).to.equal(expected, msg);
+    });
+  });
 });
 
 function createUser(config = {}) {
---
+++ functions/test/integration/deficiencies/update-progress-note-v2.test.js
@@ -0,0 +1,164 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const uuid = require('../../../test-helpers/uuid');
+const mocking = require('../../../test-helpers/mocking');
+const systemModel = require('../../../models/system');
+const usersModel = require('../../../models/users');
+const createHandler = require('../../../deficient-items/on-update-progress-note-v2');
+
+describe('Deficiencies | On Update Progress Note V2', function() {
+  afterEach(() => sinon.restore());
+
+  it('should not publish comment when progress notes unchanged', async () => {
+    const expected = false;
+    const userId = uuid();
+    const propertyId = uuid();
+    const itemId = uuid();
+    const inspectionId = uuid();
+    const deficiencyId = uuid();
+    const progNoteId = uuid();
+    const deficiencyData = mocking.createDeficiency({
+      property: propertyId,
+      inspection: inspectionId,
+      item: itemId,
+      progressNotes: {
+        [progNoteId]: {
+          startDate: '1/11/25',
+          progressNote: 'text',
+          user: userId,
+          createdAt: now(),
+        },
+      },
+    });
+
+    const trelloCardLookup = sinon
+      .stub(systemModel, 'firestoreFindTrelloCardId')
+      .resolves();
+
+    await createHandler(stubFirestore())(
+      {
+        before: createSnapshot(deficiencyId, deficiencyData),
+        after: createSnapshot(deficiencyId, deficiencyData),
+      },
+      { params: { deficiencyId } }
+    );
+
+    const actual = trelloCardLookup.called;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should not publish comment when deficiency does not have a Trello card', async () => {
+    const expected = false;
+    const userId = uuid();
+    const propertyId = uuid();
+    const itemId = uuid();
+    const inspectionId = uuid();
+    const deficiencyId = uuid();
+    const progNoteId = uuid();
+    const beforeDefData = mocking.createDeficiency({
+      property: propertyId,
+      inspection: inspectionId,
+      item: itemId,
+      progressNotes: {
+        [progNoteId]: {
+          startDate: '1/11/25',
+          progressNote: 'text',
+          user: userId,
+          createdAt: now(),
+        },
+      },
+    });
+    // Add new progress note
+    const afterDefData = JSON.parse(JSON.stringify(beforeDefData));
+    afterDefData.progressNotes[uuid()] = {
+      startDate: '1/11/25',
+      progressNote: 'text',
+      user: userId,
+      createdAt: now() + 1,
+    };
+
+    sinon.stub(systemModel, 'firestoreFindTrelloCardId').resolves('');
+    const userLookup = sinon.stub(usersModel, 'firestoreFindRecord').resolves();
+
+    await createHandler(stubFirestore())(
+      {
+        before: createSnapshot(deficiencyId, beforeDefData),
+        after: createSnapshot(deficiencyId, afterDefData),
+      },
+      { params: { deficiencyId } }
+    );
+
+    const actual = userLookup.called;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should not publish Trello card comment when Progress Note missing author', async () => {
+    const expected = false;
+    const userId = uuid();
+    const cardId = uuid();
+    const propertyId = uuid();
+    const itemId = uuid();
+    const inspectionId = uuid();
+    const deficiencyId = uuid();
+    const progNoteId = uuid();
+    const beforeDefData = mocking.createDeficiency({
+      property: propertyId,
+      inspection: inspectionId,
+      item: itemId,
+      progressNotes: {
+        [progNoteId]: {
+          startDate: '1/11/25',
+          progressNote: 'text',
+          user: userId,
+          createdAt: now(),
+        },
+      },
+    });
+    // Add new progress note
+    const afterDefData = JSON.parse(JSON.stringify(beforeDefData));
+    afterDefData.progressNotes[uuid()] = {
+      startDate: '1/11/25',
+      progressNote: 'text',
+      user: userId,
+      createdAt: now() + 1,
+    };
+
+    sinon.stub(systemModel, 'firestoreFindTrelloCardId').resolves(cardId);
+    sinon
+      .stub(usersModel, 'firestoreFindRecord')
+      .resolves(createSnapshot(userId));
+    const credentialsLookup = sinon
+      .stub(systemModel, 'firestoreFindTrello')
+      .resolves({});
+
+    await createHandler(stubFirestore())(
+      {
+        before: createSnapshot(deficiencyId, beforeDefData),
+        after: createSnapshot(deficiencyId, afterDefData),
+      },
+      { params: { deficiencyId } }
+    );
+
+    const actual = credentialsLookup.called;
+    expect(actual).to.equal(expected);
+  });
+});
+
+function stubFirestore() {
+  return {
+    collection: () => {},
+    batch: () => ({}),
+  };
+}
+
+function createSnapshot(id = uuid(), data = null) {
+  return {
+    exists: Boolean(data),
+    id,
+    data: () => data,
+  };
+}
+
+function now() {
+  return Math.round(Date.now() / 1000);
+}
