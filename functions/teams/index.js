034e8128327a1eb4086fcef957a8c0373461f209
Matthew Jensen <matt@jwc.works>
2020-08-03 15:39:44 -0500

    #484 remove team delete firebase watcher

--- functions/teams/index.js
+++ functions/teams/index.js
@@ -1,4 +1,3 @@
-const createOnDeleteWatcher = require('./on-delete-watcher');
 const onDeleteV2 = require('./on-delete-v2');
 const removeForProperty = require('./utils/remove-for-property');
 const createSyncTeam = require('./pubsub/team-sync');
@@ -6,7 +5,6 @@ const createSyncUserTeam = require('./pubsub/user-teams-sync');
 
 module.exports = {
   removeForProperty,
-  createOnDeleteWatcher,
   onDeleteV2,
 
   pubsub: {
