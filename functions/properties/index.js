5f8c0b4a898d7f64d285ef4c213b87e0ce964d40
Matthew Jensen <matt@jwc.works>
2020-08-03 15:31:00 -0500

    #484 remove property write firebase watcher

--- functions/properties/index.js
+++ functions/properties/index.js
@@ -1,8 +1,6 @@
-const createOnWriteWatcher = require('./on-write-watcher');
 const onWriteV2 = require('./on-write-v2');
 const createOnDeleteWatcher = require('./on-delete-watcher');
 const onDeleteWatcherV2 = require('./on-delete-watcher-v2');
-const createOnWriteTemplatesWatcher = require('./on-write-templates-watcher');
 const createOnWriteTeamsWatcher = require('./on-write-team-watcher');
 const getPropertyYardiResidents = require('./api/get-property-yardi-residents');
 const getPropertyYardiWorkOrders = require('./api/get-property-yardi-work-orders');
@@ -15,9 +13,7 @@ const processMeta = require('./utils/process-meta');
 module.exports = {
   createOnDeleteWatcher,
   onDeleteWatcherV2,
-  createOnWriteWatcher,
   onWriteV2,
-  createOnWriteTemplatesWatcher,
   createOnWriteTeamsWatcher,
   pubsub: { createSyncMeta },
   api: {
