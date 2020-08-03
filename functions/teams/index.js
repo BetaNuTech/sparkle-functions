7d9f0ee8a1f6a9587bd965d2dcd4b3afafc8f112
Matthew Jensen <matt@jwc.works>
2020-08-03 15:38:30 -0500

    #484 remove teams user write firebase watcher

--- functions/teams/index.js
+++ functions/teams/index.js
@@ -1,6 +1,5 @@
 const createOnDeleteWatcher = require('./on-delete-watcher');
 const onDeleteV2 = require('./on-delete-v2');
-const createOnWriteUserTeamWatcher = require('./on-write-user-teams-watcher');
 const removeForProperty = require('./utils/remove-for-property');
 const createSyncTeam = require('./pubsub/team-sync');
 const createSyncUserTeam = require('./pubsub/user-teams-sync');
@@ -9,7 +8,6 @@ module.exports = {
   removeForProperty,
   createOnDeleteWatcher,
   onDeleteV2,
-  createOnWriteUserTeamWatcher,
 
   pubsub: {
     createSyncTeam,
