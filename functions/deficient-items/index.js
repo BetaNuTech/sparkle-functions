9b3b606e55c523c17e88bc3d7bdfc6fbc45abcff
Matthew Jensen <matt@jwc.works>
2020-08-03 15:49:42 -0500

    #484 remove deficient item archiving/unarchiving firebase watchers

--- functions/deficient-items/index.js
+++ functions/deficient-items/index.js
@@ -1,5 +1,4 @@
 const createOnUpdateStateV2 = require('./on-update-state-watcher-v2');
-const createOnUpdateArchive = require('./on-update-archive-watcher');
 const createOnUpdateArchiveV2 = require('./on-update-archive-watcher-v2');
 const onUpdateProgressNote = require('./on-update-progress-note');
 const onUpdateProgressNoteV2 = require('./on-update-progress-note-v2');
@@ -12,7 +11,6 @@ const syncOverdue = require('./pubsub/sync-overdue-v2');
 
 module.exports = {
   createOnUpdateStateV2,
-  createOnUpdateArchive,
   createOnUpdateArchiveV2,
   onUpdateProgressNote,
   onUpdateProgressNoteV2,
