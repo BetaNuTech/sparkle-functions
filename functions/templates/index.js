6a11fc10f5ffbfbe4c18d89c8ad4825d904641a2
Matthew Jensen <matt@jwc.works>
2020-08-04 09:56:40 -0500

    #484 remove template write watcher

--- functions/templates/index.js
+++ functions/templates/index.js
@@ -1,10 +1,7 @@
-const createOnWriteWatcher = require('./on-write-watcher');
 const createSyncTemplatesList = require('./pubsub/sync-templates-list');
 const createSyncPropertyTemplatesList = require('./pubsub/sync-property-templates-list');
 
 module.exports = {
-  createOnWriteWatcher,
-
   pubsub: {
     createSyncTemplatesList,
     createSyncPropertyTemplatesList,
