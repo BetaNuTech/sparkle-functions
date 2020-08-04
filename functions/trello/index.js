222ed217ac936ff30ae60f6208ab95b3832c84a8
Matthew Jensen <matt@jwc.works>
2020-08-04 11:32:24 -0500

    #488 remove old trello token POST endpoint

--- functions/trello/index.js
+++ functions/trello/index.js
@@ -1,4 +1,3 @@
-const createOnUpsertTrelloTokenHandler = require('./on-create-request-handler');
 const createOnGetAllTrelloBoardsHandler = require('./get-all-trello-boards-handler');
 const createOnGetAllTrelloBoardListsHandler = require('./get-all-trello-board-lists-handler');
 const createOnTrelloDeficientItemCardHandler = require('./create-trello-deficient-item-card-handler');
@@ -13,7 +12,6 @@ const getBoardLists = require('./api/get-board-lists');
 const postDeficiencyCard = require('./api/post-deficiency-card');
 
 module.exports = {
-  createOnUpsertTrelloTokenHandler,
   createOnGetAllTrelloBoardsHandler,
   createOnGetAllTrelloBoardListsHandler,
   createOnTrelloDeficientItemCardHandler,
