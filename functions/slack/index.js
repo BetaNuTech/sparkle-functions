ab5294b413cd03f697e78a7114d18bbe409484e6
Matthew Jensen <mjens02@gmail.com>
2020-08-04 16:37:27 -0500

    #488 remove old slack app event webhook

--- functions/slack/index.js
+++ functions/slack/index.js
@@ -1,11 +1,8 @@
-const slackEventsApiHandler = require('./slack-events-api-handler');
 const postAuth = require('./api/post-auth');
 const deleteAuth = require('./api/delete-auth');
 const postEventsWebhook = require('./api/post-events-webhook');
 
 module.exports = {
-  slackEventsApiHandler,
-
   api: {
     postAuth,
     deleteAuth,
