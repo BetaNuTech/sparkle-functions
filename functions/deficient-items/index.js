3b7c650cc64c12f2d5654438f4712f899f108d6c
Matthew Jensen <matt@jwc.works>
2020-08-04 09:44:13 -0500

    #484 remove firebase deficient item progress note watcher

--- functions/deficient-items/index.js
+++ functions/deficient-items/index.js
@@ -1,6 +1,5 @@
 const createOnUpdateStateV2 = require('./on-update-state-watcher-v2');
 const createOnUpdateArchiveV2 = require('./on-update-archive-watcher-v2');
-const onUpdateProgressNote = require('./on-update-progress-note');
 const onUpdateProgressNoteV2 = require('./on-update-progress-note-v2');
 const onUpdateCompletedPhotoV2 = require('./on-update-completed-photo-v2');
 const trelloCardStateComment = require('./pubsub/trello-card-state-comment-v2');
@@ -12,7 +11,6 @@ const syncOverdue = require('./pubsub/sync-overdue-v2');
 module.exports = {
   createOnUpdateStateV2,
   createOnUpdateArchiveV2,
-  onUpdateProgressNote,
   onUpdateProgressNoteV2,
   onUpdateCompletedPhotoV2,
   pubsub: {
