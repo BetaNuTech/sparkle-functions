10252ef01318e53f8fa6f6a569f1daf09a02e403
Matthew Jensen <mjens02@gmail.com>
2020-08-05 11:10:07 -0500

    #485 remove inspection proxy cleanup subscribers

--- functions/inspections/index.js
+++ functions/inspections/index.js
@@ -2,19 +2,12 @@ const processWrite = require('./process-write');
 const onWriteV2 = require('./on-write-v2');
 const onDeleteV2 = require('./on-delete-v2');
 const createAPIPatchProperty = require('./api/patch-property');
-const createCleanupProxyOrphans = require('./pubsub/cleanup-proxy-orphans');
-const createSyncCompletedInspectionProxies = require('./pubsub/sync-completed-inspection-proxies');
 const createAPIGetInspectionPDF = require('./on-get-pdf-report/get-pdf-handler');
 
 module.exports = {
   processWrite,
   onDeleteV2,
   onWriteV2,
-
-  pubsub: {
-    createCleanupProxyOrphans,
-    createSyncCompletedInspectionProxies,
-  },
 
   api: {
     createPatchProperty: createAPIPatchProperty,
