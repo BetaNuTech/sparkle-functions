8aa0de2f06fb30ce7213ab4f7ed548b22fe6d235
Matthew Jensen <mjens02@gmail.com>
2020-08-05 10:51:57 -0500

    #485 remove template list sync subscriber

--- functions/templates/index.js
+++ functions/templates/index.js
@@ -1,9 +1,7 @@
-const createSyncTemplatesList = require('./pubsub/sync-templates-list');
 const createSyncPropertyTemplatesList = require('./pubsub/sync-property-templates-list');
 
 module.exports = {
   pubsub: {
-    createSyncTemplatesList,
     createSyncPropertyTemplatesList,
   },
 };
