c5e2eb561cbf6db0dddc4f5a2b08f8b40d17148c
Matthew Jensen <mjens02@gmail.com>
2020-08-05 11:25:02 -0500

    #485 remove property meta sync subscriber

--- functions/properties/index.js
+++ functions/properties/index.js
@@ -3,7 +3,6 @@ const onDeleteWatcherV2 = require('./on-delete-watcher-v2');
 const getPropertyYardiResidents = require('./api/get-property-yardi-residents');
 const getPropertyYardiWorkOrders = require('./api/get-property-yardi-work-orders');
 const getLatestCompletedInspection = require('./api/get-latest-completed-inspection');
-const createSyncMeta = require('./pubsub/sync-meta');
 const propertyCode = require('./middleware/property-code');
 const yardiIntegration = require('./middleware/yardi-integration');
 const processMeta = require('./utils/process-meta');
@@ -11,7 +10,6 @@ const processMeta = require('./utils/process-meta');
 module.exports = {
   onDeleteWatcherV2,
   onWriteV2,
-  pubsub: { createSyncMeta },
   api: {
     getPropertyYardiResidents,
     getPropertyYardiWorkOrders,
