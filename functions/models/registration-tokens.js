d1aeda6213ea387c1467dffc184d4f1857fc4690
Matthew Jensen <mjens02@gmail.com>
2020-08-10 15:52:39 -0500

    #492 cleanup expired firestore tokens

--- functions/models/registration-tokens.js
+++ functions/models/registration-tokens.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
+const FieldValue = require('firebase-admin').firestore.FieldValue;
 const modelSetup = require('./utils/model-setup');
 
-// const PREFIX = 'models: registration-tokens:';
+const PREFIX = 'models: registration-tokens:';
 const TOKEN_COLLECTION = 'registrationTokens';
 
 module.exports = modelSetup({
@@ -35,5 +36,75 @@ module.exports = modelSetup({
       .collection(TOKEN_COLLECTION)
       .doc(userId)
       .create(data);
+  },
+
+  /**
+   * Check all registration tokens
+   * for all users and remove outdated
+   * @param  {admin.firestore} fs
+   * @param  {Number}  maxTimestamp
+   * @param  {firestore.batch?}  parentBatch
+   * @return {Promise} - {resolves} DocumentSnapshot
+   */
+  async firestoreRemoveOutdated(fs, maxTimestamp, parentBatch) {
+    assert(fs && typeof fs.collection === 'function', 'has firestore db');
+    assert(
+      typeof maxTimestamp === 'number' && maxTimestamp === maxTimestamp,
+      'has max timestamp'
+    );
+    if (parentBatch) {
+      assert(
+        typeof parentBatch.update === 'function',
+        'has valid firestore batch'
+      );
+    }
+
+    const batch = parentBatch || fs.batch();
+
+    // Lookup all registration tokens
+    let tokensSnap = null;
+    try {
+      tokensSnap = await fs.collection(TOKEN_COLLECTION).get();
+    } catch (err) {
+      throw Error(
+        `${PREFIX} firestoreRemoveOutdated: Failed to lookup all tokens: ${err}`
+      );
+    }
+
+    // Append batched update for all of
+    // each user's outdated timestamps
+    tokensSnap.forEach(userTokensSnap => {
+      const userTokensData = userTokensSnap.data() || {};
+      const tokenIds = Object.keys(userTokensData);
+      const expiredTokenRemovals = tokenIds.reduce((acc, tokenId) => {
+        const tokenCreationDate = userTokensData[tokenId];
+        if (
+          typeof tokenCreationDate === 'number' &&
+          tokenCreationDate <= maxTimestamp
+        ) {
+          acc[tokenId] = FieldValue.delete();
+        }
+        return acc;
+      }, {});
+
+      // Append any updates to batch
+      if (Object.keys(expiredTokenRemovals).length) {
+        batch.update(userTokensSnap.ref, expiredTokenRemovals);
+      }
+    });
+
+    // Commit batch when
+    // not part of parent batch
+    if (!parentBatch) {
+      try {
+        await batch.commit();
+      } catch (err) {
+        throw Error(
+          `${PREFIX} firestoreRemoveOutdated: Failed to commit batch: ${err}`
+        );
+      }
+    }
+
+    return Promise.resolve(tokensSnap);
   },
 });
---
+++ functions/test/end-to-end/reg-tokens/pubsub-clean-outdated.test.js
@@ -0,0 +1,56 @@
+const { expect } = require('chai');
+const uuid = require('../../../test-helpers/uuid');
+const tokensModel = require('../../../models/registration-tokens');
+const { cleanDb } = require('../../../test-helpers/firebase');
+const { fs, test, cloudFunctions } = require('../../setup');
+
+const ONE_MONTHS_SEC = 2629805;
+const SIX_MONTHS_SEC = 15778800;
+const TWENTY_SEVEN_DAYS_SEC = 2332800;
+
+describe('Reg Token | Pubsub | Sync Outdated', () => {
+  afterEach(() => cleanDb(null, fs));
+
+  it('should remove registration tokens older than 1 month', async () => {
+    const nowUnix = Math.round(Date.now() / 1000);
+    const userId = uuid();
+    const deletedTokenId = uuid();
+    const deletedTokenId2 = uuid();
+    const unchangedTokenId = uuid();
+
+    // Setup database
+    await tokensModel.firestoreCreateRecord(fs, userId, {
+      [deletedTokenId]: nowUnix - ONE_MONTHS_SEC,
+      [deletedTokenId2]: nowUnix - SIX_MONTHS_SEC,
+      [unchangedTokenId]: nowUnix - TWENTY_SEVEN_DAYS_SEC,
+    });
+
+    // Execute
+    await test.wrap(cloudFunctions.regTokensSync)();
+
+    // Results
+    const resultSnap = await tokensModel.firestoreFindRecord(fs, userId);
+    const results = resultSnap.data();
+
+    // Assertions
+    [
+      {
+        actual: Boolean(results[deletedTokenId]),
+        expected: false,
+        msg: 'removed one month old token',
+      },
+      {
+        actual: Boolean(results[deletedTokenId2]),
+        expected: false,
+        msg: 'removed six month old token',
+      },
+      {
+        actual: Boolean(results[unchangedTokenId]),
+        expected: true,
+        msg: 'keep twenty seven day old token',
+      },
+    ].forEach(({ actual, expected, msg }) => {
+      expect(actual).to.equal(expected, msg);
+    });
+  });
+});
