64d54f47a3f33eb8a8221627d87cfbe1277a9473
Matthew Jensen <mjens02@gmail.com>
2020-08-13 15:59:49 -0500

    #516 drop property templates, insp process write, and property write util

--- functions/property-templates/utils/remove-for-property.js
+++
@@ -1,9 +0,0 @@
-/**
- * Remove all template proxies belonging to a property
- * @param  {firebaseAdmin.database}
- * @param  {String} propertyId
- * @return {Promise} - resolves {Boolean} remove() results
- */
-module.exports = function removeForProperty(db, propertyId) {
-  return db.ref(`/propertyTemplatesList/${propertyId}`).remove();
-};
