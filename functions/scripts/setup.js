05ec89a9261fbeeed46db5d1cbe138cded142063
Matthew Jensen <mjens02@gmail.com>
2020-08-17 17:09:13 -0500

    #526 remove sync scripts

--- functions/scripts/setup.js
+++ functions/scripts/setup.js
@@ -15,7 +15,6 @@ const config = require('../config'); // eslint-disable-line
 const { firebase: fbConfig } = config;
 const admin = require('firebase-admin'); // eslint-disable-line
 const defaultApp = admin.initializeApp(fbConfig, 'script');
-const db = defaultApp.database(process.env.FIREBASE_DB_URL);
 const fs = defaultApp.firestore();
 const test = require('firebase-functions-test')(fbConfig); // eslint-disable-line
 const cloudFunctions = require('../index'); // eslint-disable-line
@@ -24,7 +23,6 @@ const auth = admin.auth();
 module.exports = {
   admin,
   auth,
-  db,
   fs,
   test,
   cloudFunctions,
