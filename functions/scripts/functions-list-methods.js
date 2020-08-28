ba2578c4c53a0f5a9603bbc965f735e930cab8f0
Matthew Jensen <mjens02@gmail.com>
2020-08-28 17:57:05 -0500

    remove unnecessary functions list script

--- functions/scripts/functions-list-methods.js
+++
@@ -1,7 +0,0 @@
-const { cloudFunctions } = require('./setup');
-
-const functionNames = Object.keys(cloudFunctions);
-
-// Write matched functions to STDOUT
-process.stdout.write(functionNames.map(name => `functions:${name}`).join(','));
-process.exit(0);
