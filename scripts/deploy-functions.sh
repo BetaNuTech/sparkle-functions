7534445c350289b4ec4fc15149dd5bfeeed3fa36
Matthew Jensen <mjens02@gmail.com>
2020-08-28 17:49:34 -0500

    use hack to only upsert functions

--- scripts/deploy-functions.sh
+++ scripts/deploy-functions.sh
@@ -1,9 +1,7 @@
-FIREBASE_DEPLOY_FUNCTIONS="$(node ./functions/scripts/functions-list-methods.js)"
-
 firebase functions:config:set auth.firebase=${FIREBASE_FUNCTIONS_AUTH}
 firebase functions:config:set slack.secret=${SLACK_CLIENT_SECRET}
 firebase functions:config:set aws.id=${AWS_S3_ACCESS_KEY_ID}
 firebase functions:config:set aws.key=${AWS_S3_SECRET_ACCESS_KEY}
 firebase functions:config:set web.clientdomain=${CLIENT_DOMAIN}
 if [ ! -z "$COBALT_DOMAIN" ]; then firebase functions:config:set cobalt.domain=${COBALT_DOMAIN}; fi
-firebase deploy --only functions --non-interactive --token $FIREBASE_TOKEN -f
+echo "n\n" | firebase deploy --only functions --interactive --token $FIREBASE_TOKEN
