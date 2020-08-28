f9cb578af037bb0e59dd848aebd54437d44a872f
Matthew Jensen <mjens02@gmail.com>
2020-08-28 17:40:33 -0500

    force deploy

--- scripts/deploy-functions.sh
+++ scripts/deploy-functions.sh
@@ -6,4 +6,4 @@ firebase functions:config:set aws.id=${AWS_S3_ACCESS_KEY_ID}
 firebase functions:config:set aws.key=${AWS_S3_SECRET_ACCESS_KEY}
 firebase functions:config:set web.clientdomain=${CLIENT_DOMAIN}
 if [ ! -z "$COBALT_DOMAIN" ]; then firebase functions:config:set cobalt.domain=${COBALT_DOMAIN}; fi
-firebase deploy --only functions --non-interactive --token $FIREBASE_TOKEN
+firebase deploy --only functions --non-interactive --token $FIREBASE_TOKEN -f
