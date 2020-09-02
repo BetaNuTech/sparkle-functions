91600eca7116556b23629a1a0631f374e97ac33c
Matthew Jensen <mjens02@gmail.com>
2020-09-02 16:53:28 -0500

    #548 fix global api payload

--- functions/services/global-api.js
+++ functions/services/global-api.js
@@ -29,7 +29,7 @@ module.exports = {
           responseType: 'json',
           method: 'PATCH',
           json: true,
-          body: { slackTeam: slackTeamId || null },
+          body: { team: slackTeamId || null },
         }
       );
 
