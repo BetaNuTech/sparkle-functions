7b47132cc45e4875ee93f2c9ee74bddcea460d4c
Matthew Jensen <mjens02@gmail.com>
2021-05-03 12:07:20 -0500

    #623 make bucketname configurable

--- scripts/deploy-functions.sh
+++ scripts/deploy-functions.sh
@@ -2,6 +2,7 @@ firebase functions:config:set auth.firebase=${FIREBASE_FUNCTIONS_AUTH}
 firebase functions:config:set slack.secret=${SLACK_CLIENT_SECRET}
 firebase functions:config:set aws.id=${AWS_S3_ACCESS_KEY_ID}
 firebase functions:config:set aws.key=${AWS_S3_SECRET_ACCESS_KEY}
+firebase functions:config:set aws.bucketName=${AWS_S3_BUCKET_NAME}
 firebase functions:config:set web.clientdomain=${CLIENT_DOMAIN}
 firebase functions:config:set globalApi.token=${GLOBAL_API_TOKEN}
 if [ ! -z "$COBALT_DOMAIN" ]; then firebase functions:config:set cobalt.domain=${COBALT_DOMAIN}; fi
