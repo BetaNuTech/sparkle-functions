c70733234bda5ccb14301fc165f8868d97087bab
Matthew Jensen <mjens02@gmail.com>
2021-05-09 10:59:10 -0500

    #632 fix global api config naming

--- scripts/deploy-functions.sh
+++ scripts/deploy-functions.sh
@@ -4,7 +4,7 @@ firebase functions:config:set aws.id=${AWS_S3_ACCESS_KEY_ID}
 firebase functions:config:set aws.key=${AWS_S3_SECRET_ACCESS_KEY}
 firebase functions:config:set aws.bucketName=${AWS_S3_BUCKET_NAME}
 firebase functions:config:set web.clientdomain=${CLIENT_DOMAIN}
-firebase functions:config:set globalApi.token=${GLOBAL_API_TOKEN}
-firebase functions:config:set globalApi.domain=${GLOBAL_API_DOMAIN}
+firebase functions:config:set globalapi.token=${GLOBAL_API_TOKEN}
+firebase functions:config:set globalapi.domain=${GLOBAL_API_DOMAIN}
 if [ ! -z "$COBALT_DOMAIN" ]; then firebase functions:config:set cobalt.domain=${COBALT_DOMAIN}; fi
 echo "n\n" | firebase deploy --only functions --interactive --token $FIREBASE_TOKEN
