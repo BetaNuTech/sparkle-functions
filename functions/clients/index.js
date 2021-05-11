49d1bac892981f9d8b2a96aa3d7fa412101e05de
Matthew Jensen <mjens02@gmail.com>
2021-05-10 20:04:45 -0500

    #630 add post error reporting endpoint

--- functions/clients/index.js
+++ functions/clients/index.js
@@ -1,5 +1,6 @@
 const getAppVersions = require('./api/get-app-versions');
+const postError = require('./api/post-error');
 
 module.exports = {
-  api: { getAppVersions },
+  api: { getAppVersions, postError },
 };
