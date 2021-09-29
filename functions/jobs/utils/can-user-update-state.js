b123dd642c334e48cf9452dc7f8c3e2fe780b44a
Matthew Jensen <mjens02@gmail.com>
2021-09-29 17:07:51 -0500

    #724 update for new authorize job rules

--- functions/jobs/utils/can-user-update-state.js
+++ functions/jobs/utils/can-user-update-state.js
@@ -12,7 +12,11 @@ const assert = require('assert');
 module.exports = (targetState, job, bids, user) => {
   assert(targetState && typeof targetState === 'string', 'has targeted state');
   assert(job && typeof job === 'object', 'has job record');
-  assert(bids && typeof bids === 'object', 'has bids record');
+  assert(bids && Array.isArray(bids), 'has bids array');
+  assert(
+    bids.every(bid => typeof bid === 'object'),
+    'has array of bid objects'
+  );
   assert(user && typeof user === 'object', 'has user record');
 
   switch (targetState) {
@@ -27,22 +31,22 @@ module.exports = (targetState, job, bids, user) => {
         (total, bid) => total + (bid.state === 'approved' ? 1 : 0),
         0
       );
-      // Regular job
-      if (approvedBids >= 1 && bids.length >= job.minBids) {
-        return true;
-      }
+      const minBids = job.minBids || Infinity;
+      const hasMetApprovedBidReq = approvedBids >= 1;
+      const hasMetBidRequirement = bids.length >= minBids;
 
       // Expedited job
-      if (
-        user.admin &&
-        job.authorizedRules === 'expedite' &&
-        approvedBids >= 1 &&
-        bids.length >= job.minBids
-      ) {
-        return true;
+      if (job.authorizedRules === 'expedite') {
+        return user.admin && hasMetBidRequirement && hasMetBidRequirement;
       }
 
-      return false;
+      // Large job
+      if (job.authorizedRules === 'large') {
+        return user.admin && hasMetBidRequirement && hasMetBidRequirement;
+      }
+
+      // Default job
+      return hasMetApprovedBidReq && hasMetBidRequirement;
     }
     case 'complete': {
       if (job.state === 'authorized') return true;
