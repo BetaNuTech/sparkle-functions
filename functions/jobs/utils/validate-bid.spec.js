06cf8039d12297b7783d1a4cad222645c1b33fba
Matthew Jensen <mjens02@gmail.com>
2021-10-08 12:10:41 -0500

    #736 updated bid validation logic to allow unsetting and returning all errors

--- functions/jobs/utils/validate-bid.spec.js
+++ functions/jobs/utils/validate-bid.spec.js
@@ -9,22 +9,11 @@ const requiredAttrs = {
 
 describe('Jobs | Utils | Validate Bid Create', () => {
   it('rejects if required attribute is not provided in schema', () => {
-    const expected = 'vendor,scope is required';
+    const expected = ['scope', 'vendor'];
     const result = validate({
       vendorDetails: 'test',
     });
-    const paths = result.map(r => r.path).join(',');
-    const actual = `${paths} is required`;
-    expect(actual).to.equal(expected);
-  });
-
-  it('rejects if non existing bid attribute is provided', () => {
-    const expected = [];
-    const actual = validate({
-      ...requiredAttrs,
-      invalid: 'non-exixting',
-    });
-
+    const actual = getResults(result);
     expect(actual).to.deep.equal(expected);
   });
 
@@ -70,74 +59,177 @@ describe('Jobs | Utils | Validate Bid Create', () => {
     for (let i = 0; i < data.length; i++) {
       const { bid, expected, msg } = data[i];
       const result = validate({ vendor: 'test', ...bid });
-      const actual = result.map(err => err.path).join(',');
-      expect(actual).to.equal(expected, msg);
+      const actual = getResults(result).join(',');
+      expect(actual).to.deep.equal(expected, msg);
     }
   });
 
   it('rejects if started at is greater than completed at', () => {
-    const expected = [
-      'Validation failed for startAt.',
-      'Validation failed for completeAt.',
-    ];
+    const expected = ['completeAt', 'startAt'];
     const result = validate({
       ...requiredAttrs,
       completeAt: 1,
       startAt: 2,
     });
-    const actual = [];
-
-    result.map(({ message }) => actual.push(message)).join(',');
-
+    const actual = getResults(result);
     expect(actual).to.deep.equal(expected);
   });
 
   it('rejects if cost min is greater than cost max', () => {
-    const expected = 'costMax,costMin';
+    const expected = ['costMax', 'costMin'];
     const result = validate({
       ...requiredAttrs,
       costMin: 2,
       costMax: 1,
     });
-    const actual = result
-      .map(({ path }) => path)
-      .sort()
-      .join(',');
+    const actual = getResults(result);
     expect(actual).to.deep.equal(expected);
   });
 
   it('accepts a fixed cost bid', () => {
-    const expected = '';
+    const expected = [];
     const result = validate({
       ...requiredAttrs,
       costMin: 1,
       costMax: 1,
     });
-    const actual = result
-      .map(({ path }) => path)
-      .sort()
-      .join(',');
+    const actual = getResults(result);
     expect(actual).to.deep.equal(expected);
   });
 
   it('rejects if completed at is greater than started at', () => {
-    const expected = 'completeAt,startAt';
+    const expected = ['completeAt', 'startAt'];
     const result = validate({
       ...requiredAttrs,
       completeAt: 1,
       startAt: 2,
     });
-    const actual = result
-      .map(({ path }) => path)
-      .sort()
-      .join(',');
+    const actual = getResults(result);
+    expect(actual).to.deep.equal(expected);
+  });
 
+  it('accepts a valid min cost, even when max cost is unset', () => {
+    const expected = [];
+    const result = validate({
+      ...requiredAttrs,
+      costMin: 1,
+      costMax: undefined,
+    });
+    const actual = getResults(result);
+    expect(actual).to.deep.equal(expected);
+  });
+
+  it('accepts a valid max cost, even when min cost is unset', () => {
+    const expected = [];
+    const result = validate({
+      ...requiredAttrs,
+      costMax: 1,
+      costMin: undefined,
+    });
+    const actual = getResults(result);
+    expect(actual).to.deep.equal(expected);
+  });
+
+  it('rejects if completed at is less than started at', () => {
+    const expected = ['completeAt', 'startAt'];
+    const result = validate({
+      ...requiredAttrs,
+      completeAt: 1,
+      startAt: 2,
+    });
+    const actual = getResults(result);
+    expect(actual).to.deep.equal(expected);
+  });
+
+  it('accepts unsetting both completed at and start at times', () => {
+    const expected = [];
+    const tests = [
+      {
+        data: {
+          ...requiredAttrs,
+          completeAt: 0,
+          startAt: 1,
+        },
+        msg: 'unset completed at with valid start at',
+      },
+      {
+        data: {
+          ...requiredAttrs,
+          completeAt: 1,
+          startAt: 0,
+        },
+        msg: 'unset started at with valid completed at',
+      },
+      {
+        data: {
+          ...requiredAttrs,
+          completeAt: 0,
+          startAt: 0,
+        },
+        msg: 'unset started at and completed at together',
+      },
+    ];
+
+    for (let i = 0; i < tests.length; i++) {
+      const { data, msg } = tests[i];
+      const actual = getResults(validate(data));
+      expect(actual).to.deep.equal(expected, msg);
+    }
+  });
+
+  it('accepts unsetting both min and max costs', () => {
+    const expected = [];
+    const tests = [
+      {
+        data: {
+          ...requiredAttrs,
+          costMax: 0,
+          costMin: 1,
+        },
+        msg: 'unset max cost with valid min cost',
+      },
+      {
+        data: {
+          ...requiredAttrs,
+          costMax: 1,
+          costMin: 0,
+        },
+        msg: 'unset min cost with valid max cost',
+      },
+      {
+        data: {
+          ...requiredAttrs,
+          costMax: 0,
+          costMin: 0,
+        },
+        msg: 'unset min cost and max cost together',
+      },
+    ];
+
+    for (let i = 0; i < tests.length; i++) {
+      const { data, msg } = tests[i];
+      const actual = getResults(validate(data));
+      expect(actual).to.deep.equal(expected, msg);
+    }
+  });
+
+  it('rejects with all errors for completed at, started at, cost min, and cost max', () => {
+    const expected = ['completeAt', 'costMax', 'costMin', 'startAt'];
+    const result = validate({
+      ...requiredAttrs,
+      costMin: 2,
+      costMax: 1,
+      startAt: 2,
+      completeAt: 1,
+    });
+
+    const actual = getResults(result);
     expect(actual).to.deep.equal(expected);
   });
 
   it('accpets a valid bid', () => {
     const expected = [];
-    const actual = validate({
+    const result = validate({
       vendor: 'test',
       vendorDetails: 'test',
       costMin: 1,
@@ -147,6 +239,16 @@ describe('Jobs | Utils | Validate Bid Create', () => {
       scope: config.bids.scopeTypes[0],
     });
 
+    const actual = getResults(result);
     expect(actual).to.deep.equal(expected);
   });
 });
+
+/**
+ * Flatted validate results to path pointers
+ * @param  {Object[]}} validateResult
+ * @return {String[]}
+ */
+function getResults(validateResult) {
+  return validateResult.map(r => r.path).sort();
+}
