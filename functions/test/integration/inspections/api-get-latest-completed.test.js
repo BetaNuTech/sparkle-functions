6e8a7a8940c4dc85529e5675cfa8092d4f86dc9c
Stu Carney <webstu@gmail.com>
2022-06-02 15:38:12 -0500

    #855 Fixed Integration Test

--- functions/test/integration/inspections/api-get-latest-completed.test.js
+++ functions/test/integration/inspections/api-get-latest-completed.test.js
@@ -10,6 +10,7 @@ const inspectionsModel = require('../../../models/inspections');
 const getLatest = require('../../../inspections/api/get-latest-completed');
 
 const INSP_URL_PATH = config.clientApps.web.inspectionURL;
+const DEFICIENT_ITEMS_URL_PATH = config.clientApps.web.deficientItemsURL;
 const TODAY_UNIX = Math.round(Date.now() / 1000);
 
 describe('Inspections | API | GET Latest Completed', () => {
@@ -173,6 +174,7 @@ describe('Inspections | API | GET Latest Completed', () => {
           templateName: inspection.templateName,
           inspectionReportURL: inspection.inspectionReportURL,
           inspectionURL: createInspectionUrl(inspection.property, 'expected'),
+          deficientItemsURL: createDeficientItemsUrl(inspection.property, 'expected'),
         },
       },
     };
@@ -331,3 +333,8 @@ function createInspectionUrl(propertyId, inspId) {
     inspId
   );
 }
+
+function createDeficientItemsUrl(propertyId) {
+  return DEFICIENT_ITEMS_URL_PATH.replace('{{propertyId}}', propertyId);
+}
+
